14dfcce0508bed8c50d8c84fe4049e27bd3fbeb4
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-07-31 20:32:19 +0530

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {    
         map<int,int> nMap;
-
-        int n = nums.size();
-        for(int i=0; i<n; i++){
+        
+        for(int i=0; i<nums.size(); i++){
             int complement = target - nums[i];
 
             if(nMap.find(complement) != nMap.end()) return{nMap[complement], i};
