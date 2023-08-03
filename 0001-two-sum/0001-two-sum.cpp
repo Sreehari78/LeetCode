89a27852006b645a216226dba4ebf1c40f7b6a06
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-08-03 20:24:31 +0530

    Time: 10 ms (85.77%), Space: 11.1 MB (15.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {    
         map<int,int> nMap;
-        
+
         for(int i=0; i<nums.size(); i++){
             int complement = target - nums[i];
 
