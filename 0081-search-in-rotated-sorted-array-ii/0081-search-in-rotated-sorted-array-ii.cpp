bfbec285c828a3bd2bab6ba3a54d254dea3b99d2
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-08-29 12:59:08 +0530

    Time: 5 ms (50.09%), Space: 13.9 MB (65.05%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,37 +1,29 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        
-        return newSearch(nums,target,0,nums.size()-1);
- 
-    }
-    bool newSearch(vector<int>& nums, int target,int start,int end)
-    {
-        int mid=start+(end-start)/2;
-        if(start>end) return false;
-        if(nums[mid] == target) return true;
-        if(nums[start] == target) return true;
-        if(nums[end] == target) return true;
-        if(nums[mid]==nums[start]||nums[mid]==nums[end])
-        {
-            if(!newSearch(nums,target,start,mid-1))
-                return newSearch(nums,target,mid + 1,end);
-            return true;
+        int start = 0, end = nums.size() - 1, mid = 0;
+
+        while(start <= end) {
+            mid = (start + end) / 2;
+
+            if(target == nums[mid]) return true;
+
+            if(nums[start] == nums[mid] && nums[mid] == nums[end]) {
+                start++;
+                end--;
+                continue;
+            }
+            
+            if(nums[start] <= nums[mid]) {   //check if left half is sorted
+                if(nums[start] <= target && target < nums[mid]) end = mid - 1;
+                else start = mid + 1;
+            }
+
+            if(nums[mid] <= nums[end]) {   //check if right half is sorted
+                if(nums[end] >= target && target > nums[mid]) start = mid + 1;
+                else end = mid - 1;
+            }
         }
-        if(nums[mid] > nums[start]){
-            if(target < nums[mid] && target >= nums[start])
-                return binary_search(nums.begin()+start,nums.begin()+mid,target);
-            else
-                return newSearch(nums,target,mid + 1,end);
-            }
-        else{
-            if(target > nums[mid] && target <= nums[end])
-                return binary_search(nums.begin()+mid+1,nums.begin()+end,target);
-            else
-                return newSearch(nums,target,start,mid-1);
-            }
+        return false;
     }
 };
