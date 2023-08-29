d6fee636733eef0cbb5cbd5c1b3ff1885c6c12eb
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-08-29 15:08:10 +0530

    Time: 45 ms (99.63%), Space: 59.6 MB (5.33%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
         
         int area = 0, ht = 0, start = 0, end = height.size() - 1;
 
