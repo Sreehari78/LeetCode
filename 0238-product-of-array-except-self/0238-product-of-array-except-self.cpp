7a5f92d176dc41cdf677b4fa81700ba07a8880fc
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-01 19:17:11 +0530

    Time: 10 ms (98.82%), Space: 25 MB (39.49%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,29 +5,19 @@ public:
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
 
-        int product = 1, flag = 0, val = 0;
-        vector<int> answer, ans;
-        for(int i = 0; i < nums.size(); i++) {
-            ans.push_back(0);
-            if(nums[i] == 0)
-                answer.push_back(0);
-        }
-        
-        if(answer.size() == nums.size()) return answer;
-        if(answer.size() > 1) return ans;
-
+        int product = 1, c = 0;
+        vector<int> answer;
         
         for(int i = 0; i < nums.size(); i++)
             if(nums[i] != 0) product *= nums[i];
-            else flag = 1;
+            else c++;
 
         answer.clear();
 
         for(int i = 0; i < nums.size(); i++){
-            if(!flag) val = product / nums[i];
-            else if(nums[i] == 0) val = product;
-            else val = 0;
-            answer.push_back(val);
+            if(!c) answer.push_back(product / nums[i]);
+            else if(c == 1 && nums[i] == 0) answer.push_back(product);
+            else answer.push_back(0);
         }
 
         return answer;
