8676cd14669893c78a0eb061dc17e3d3dfe10427
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-04 21:43:03 +0530

    Time: 0 ms (100%), Space: 5.8 MB (91.95%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     double myPow(double x, long n) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        
         if(n < 0) {
             x = 1/x;
             n *= -1;
