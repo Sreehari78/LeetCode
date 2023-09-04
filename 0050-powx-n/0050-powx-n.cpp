dc1935064ea366cf8994424bfddd1f02c4f9bc6e
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-04 21:41:14 +0530

    Time: 3 ms (39.65%), Space: 5.8 MB (91.95%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     double myPow(double x, long n) {
-        
-        if(n == 0 || x == 1) return 1;
-
-        if(x == -1) {
-            if(n % 2 == 0) return x*= -1;
-            else return x;
-        }
-
         if(n < 0) {
-            if(n <= INT_MIN && x >= 2) return 0.00000;
-
             x = 1/x;
             n *= -1;
         }
