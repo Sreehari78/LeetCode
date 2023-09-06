caeba9736312b68befeb0ef9ac59f23fdeb81412
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-07 00:18:03 +0530

    Time: 0 ms (100%), Space: 7.27 MB (82.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        
-        string str = "", str1 = "";
-        for(int i = 0; i < s.length(); i++)
-            if(isalnum(s[i])) str += tolower(s[i]);
-        
-        str1 = str;
-        int n = str.length();
-        
-        for(int i = 0; i < n / 2; i++)
-            swap(str[i], str[n - i - 1]);
-        
-        if(str == str1) return true;
-        return false;
-    }
+       int start=0;
+       int end=s.size()-1;
+       while(start<=end){
+           if(!isalnum(s[start])){start++; continue;}
+           if(!isalnum(s[end])){end--;continue;}
+           if(tolower(s[start])!=tolower(s[end]))return false;
+           else{
+               start++;
+               end--;
+           }
+       }
+       return true;
+}
 };
