c7dff6c70f0aaa25118962a90767cb142a19515e
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-07 19:53:33 +0530

    Time: 30 ms (69.04%), Space: 10.8 MB (47.94%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -8,10 +8,7 @@ public:
         unordered_map<char, int> mp;
 
         for(int i = 0; i < s.length(); i++) mp[s[i]]++;
-
-        for(int i = 0; i < s.length(); i++) {
-            if(mp[s[i]] == 1) return i;
-        }
+        for(int i = 0; i < s.length(); i++) if(mp[s[i]] == 1) return i;
 
         return -1;
     }
