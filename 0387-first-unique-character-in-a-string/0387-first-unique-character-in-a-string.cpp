93c452f861b26e4f3e7b1b5601347948138a40f8
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-07 19:55:22 +0530

    Time: 10 ms (99.64%), Space: 10.6 MB (92.78%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -5,11 +5,16 @@ public:
         cin.tie(nullptr);
         cout.tie(nullptr);
     
-        unordered_map<char, int> mp;
-
-        for(int i = 0; i < s.length(); i++) mp[s[i]]++;
-        for(int i = 0; i < s.length(); i++) if(mp[s[i]] == 1) return i;
-
+        int freq[26]={0};
+        for(int i=0;i<s.size();i++){
+            char ch = s[i];
+            freq[ch-'a']++;
+        }
+        for(int i=0;i<s.size();i++){
+            if(freq[s[i]-'a']==1){
+                return i;
+            }
+        }
         return -1;
     }
 };
