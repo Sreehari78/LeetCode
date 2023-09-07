d03dcedb305af4d460ed8598ce0ece7f3b777cf0
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-07 17:53:52 +0530

    Time: 11 ms (96.44%), Space: 23.3 MB (34.12%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+
+        int n = s.size();
+        for(int i = 0; i < n / 2; i++)
+            swap(s[i], s[n - i - 1]);
+
+        return ;
+    }
+};
