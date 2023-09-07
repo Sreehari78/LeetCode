c2b9332bf7bd0e8827eb5ca1ef5ff5000f5cf6c3
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-07 20:39:48 +0530

    Time: 12 ms (72.65%), Space: 6 MB (25.34%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -5,8 +5,9 @@ public:
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
 
-        if(n == 1) return true;
-        if(n <= 0 || n % 3 != 0) return false;
-        return isPowerOfThree(n/3);
+        // if(n == 1) return true;
+        // if(n <= 0 || n % 3 != 0) return false;
+
+        return (n > 0) && 1162261467 % n == 0;
     }
 };
