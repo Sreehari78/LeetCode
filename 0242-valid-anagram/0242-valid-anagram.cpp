2fe27d5d80e32377fc0db39e221035be4f0f58a8
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-08 13:39:04 +0530

    Time: 3 ms (98.6%), Space: 7.4 MB (55.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,7 +4,7 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);  
         cout.tie(nullptr);  
-        
+
         unordered_map<char, int> mp1, mp2;
         for(int i = 0; i < s.length() || i < t.length(); i++) {
             if(i < s.length()) mp1[s[i]]++;
