b1bc8d80f7dbccb637ce39c0614302b62a4f7df6
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-09 10:56:06 +0530

    Time: 16 ms (55.77%), Space: 18 MB (14.31%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -5,9 +5,8 @@ public:
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
 
-        int sum = 0, res = 0;
+        int sum=((nums.size()+1)*nums.size())/2, res = 0;
         for(int i = 0; i < nums.size(); i++) {
-            sum += i + 1;
             res += nums[i];
         }
 
