f595ad931ec5c15f17337bb5df4747e0fe685fcb
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-09 18:13:27 +0530

    Time: 3 ms (99.99%), Space: 19.2 MB (36.48%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
 
-        for(int i = 0, j = 0; i < nums.size(); i++)
-            if(nums[i] != 0) swap(nums[i], nums[j++]);
+        for(int i = 0, j = 0; i < nums.size(); i++) if(nums[i] != 0) swap(nums[i], nums[j++]);
     }
 };
