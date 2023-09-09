df23b55a1d5b2c0180244c43fe3d89c700d8dabd
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-09 10:53:22 +0530

    Time: 16 ms (55.77%), Space: 18.2 MB (8.97%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-         std::ios_base::sync_with_stdio(false);
+        std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
-        
-        unordered_map <int, int> mp;
-        for(int i = 0; i < nums.size(); i++) mp[nums[i]]++;
-        for(int i = 0; i <= nums.size(); i++) if(mp[i] != 1) return i;
-        return -1;
+
+        int sum = 0, res = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            sum += i + 1;
+            res += nums[i];
+        }
+
+        return sum - res;
     }
 };
