9c960aa9139daaaea1e93ef5298a7ef11e70cca6
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-10 14:30:04 +0530

    Time: 11 ms (87.94%), Space: 19.8 MB (7.69%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,12 +4,12 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
-        
+
         unordered_map<int, int> mp;
 
-        for(int i = 0; i < nums.size(); i++) {
-            mp[nums[i]]++;
-            if(mp[nums[i]] > nums.size() / 2) return nums[i];
+        for(auto it: nums) {
+            mp[it]++;
+            if(mp[it] > nums.size() / 2) return it;
         }
         return -1;
     }
