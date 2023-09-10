5602a8a1366fb3f347fe01b450ab1f3f61170cad
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-10 10:03:04 +0530

    Time: 3 ms (99.99%), Space: 22.3 MB (39.43%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -19,15 +19,9 @@ public:
 
             if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) return nums[mid];
 
-            if(nums[mid] == nums[mid - 1]) {
-                if(mid % 2 == 1) low = mid + 1;
-                else high = mid - 1;
-            }
+            if((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid % 2 == 0 && nums[mid]==nums[mid + 1])) low = mid + 1;
+            else high = mid - 1;
 
-            if(nums[mid] == nums[mid + 1]) {
-                if(mid % 2 == 0) low = mid + 1;
-                else high = mid - 1;
-            }
         }
         return mid;
     }
