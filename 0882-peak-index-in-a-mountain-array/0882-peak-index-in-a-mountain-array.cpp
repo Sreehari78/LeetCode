86d4e079f5abc3fefe9f3abc6be63e04eda2834d
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-10 13:20:46 +0530

    Time: 50 ms (99.83%), Space: 59.7 MB (42.99%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+
+        if(arr.size() == 1) return arr[0];
+
+        int low = 0, high = arr.size() - 1, mid = 0, small = INT_MAX, index = -1;
+
+        if(arr[low] > arr[low + 1]) return arr[low];
+        if(arr[high] > arr[high - 1]) return arr[high];
+        low++;
+        high--;
+
+        while(low <= high) {
+            mid = (low + high) / 2;
+
+            if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) return mid;
+
+            if(arr[mid - 1] <= arr[mid]) low = mid + 1;
+            else high = mid - 1;
+
+        }
+        return mid;
+    }
+};
