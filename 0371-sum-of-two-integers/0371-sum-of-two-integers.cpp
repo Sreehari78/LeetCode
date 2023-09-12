ad9ce89e9b9e400e870187d6f93bd254973494cc
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-12 14:04:18 +0530

    Time: 2 ms (53.64%), Space: 6.2 MB (15.19%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -4,7 +4,12 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
-        
-        return (a ^ b) + ((a & b) << 1);
+                
+        while(b) {
+            int temp = (a & b) << 1;
+            a = a ^ b;
+            b = temp;
+        }
+        return a;
     }
 };
