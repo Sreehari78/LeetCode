99a341a8aa6f3ffb97855db95295692fd81e663e
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-17 23:09:24 +0530

    Time: 3 ms (60.47%), Space: 8.6 MB (7.47%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        
         int alen = a.length(), blen = b.length(), i = 0, carry = 0;
         string ans ="";
 
         while(i < alen || i < blen || carry != 0){
-            int x =0;
+            int x = 0;
             if(i < alen && a[alen - i - 1] == '1'){
                 x = 1;
             }
