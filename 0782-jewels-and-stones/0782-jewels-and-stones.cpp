a539a3fa1e40f63cff3f9778b56c8e9e6b429e78
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-19 22:20:16 +0530

    Time: 0 ms (100%), Space: 6.9 MB (9.05%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        
+        unordered_map<char, int> mp;
+        int count = 0;
+
+        for(char it: jewels) mp[it]++;
+        for(char it: stones) if( mp[it] == 1) count++;
+
+        return count;
+    }
+};
