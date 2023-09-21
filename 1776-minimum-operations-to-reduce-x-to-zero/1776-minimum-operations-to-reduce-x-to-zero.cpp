f22def76a830afeee82be0fb499c0ab4e3a3c1b1
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-21 07:58:53 +0530

    Time: 294 ms (26.41%), Space: 168.9 MB (8.15%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -4,33 +4,34 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
         cout.tie(nullptr);
+
         unordered_map <int,int> hash;
         int n = nums.size();
         int sum{0};
+        
         for(int i = n-1;i>=0;i--)
         {
             sum += nums[i];
             hash[sum] = i;
         }
-        if(x > sum)
-            return -1;
+
+        if(x > sum) return -1;
+        
         sum = 0;
         int ans = INT_MAX;
+
         if(hash.find(x) != hash.end())
             ans = n - hash[x];
+        
         for(int i=0;i<n;i++)
         {
             sum += nums[i];
-            if(sum == x)
-            {
-                ans = min(i+1,ans);
-            }
-            if(hash.find(x - sum) == hash.end())
-                continue;
+            if(sum == x) ans = min(i+1,ans);
+            if(hash.find(x - sum) == hash.end()) continue;
             ans = min(i + 1 + n - hash[x-sum],ans);
         }
-        if(ans == INT_MAX)
-            return -1;
+
+        if(ans == INT_MAX) return -1;
         return ans;
     }
 };
