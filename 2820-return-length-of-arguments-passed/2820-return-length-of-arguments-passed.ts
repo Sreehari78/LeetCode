e9bbc20b587c35278e80de57282e4ef29b596f2e
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-09-24 23:20:47 +0530

    Time: 63 ms (12.34%), Space: 43.4 MB (8.21%) - LeetHub

--- 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.ts
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.ts
@@ -1,9 +1,3 @@
-type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
-
-function argumentsLength(...args: JSONValue[]): number {
+function argumentsLength(...args: any[]): number {
     return args.length;
 };
-
-/**
- * argumentsLength(1, 2, 3); // 3
- */
