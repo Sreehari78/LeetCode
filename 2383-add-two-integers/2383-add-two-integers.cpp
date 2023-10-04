7151f2874e5c2384e49a2a2fd505daa072c2a2a5
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-10-04 08:50:42 +0530

    Time: 2 ms (40%), Space: 6.3 MB (10.67%) - LeetHub

---
+++ 2383-add-two-integers/2383-add-two-integers.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        return num1 + num2;
+    }
+};
