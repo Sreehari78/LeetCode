5e27c2e15662f5dc291b2307e35e31754c9a6619
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-10-08 19:52:35 +0530

    Time: 3 ms (69.89%), Space: 11.1 MB (12.38%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -14,6 +14,7 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
         cout.tie(nullptr);
+        
         ListNode* ptr = head;
 
         for(int i = 0; i < n; i++) ptr = ptr -> next;
