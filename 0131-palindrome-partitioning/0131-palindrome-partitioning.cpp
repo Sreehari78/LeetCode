7da5077a1679d18674f91797d22b5b17f6c2e4ba
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-10-16 21:18:26 +0530

    Time: 104 ms (53.6%), Space: 57.9 MB (45.85%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -3,15 +3,14 @@ private:
     int n;
 
 public:
-    vector<vector<string>> res;
-    vector<string> ans;
+
 
     bool isPalindrome(int start, int end, string s) {
         while(start <= end) if(s[start++] != s[end--]) return false;
         return true;
     }
 
-    void reccursiveMethod(int index, string s) {
+    void reccursiveMethod(int index, string s, vector<string>& ans, vector<vector<string>>& res) {
         if(index == n) {
             res.push_back(ans);
             return;
@@ -20,7 +19,7 @@ public:
         for(int i = index; i < n; i++)  {
             if(isPalindrome(index, i, s)) {
                 ans.push_back(s.substr(index, i - index + 1));
-                reccursiveMethod(i + 1, s);
+                reccursiveMethod(i + 1, s, ans, res);
                 ans.pop_back();
             }
         }
@@ -30,9 +29,12 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
-        
+
+        vector<vector<string>> res;
+        vector<string> ans;
+
         n = s.length();
-        reccursiveMethod(0, s);
+        reccursiveMethod(0, s, ans, res);
         return res;
     }
 };
