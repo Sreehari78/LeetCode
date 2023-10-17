8e9b2bb88b96add8d11709b3d914a5eb67e01ede
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-10-17 07:41:21 +0530

    Time: 0 ms (100%), Space: 7.6 MB (51.04%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> ans;
+    void subsetReccursion(vector<int>& nums, int index) {
+        if(index >= nums.size()) {
+            res.push_back(ans);
+            return;
+        }
+
+        ans.push_back(nums[index]);
+        subsetReccursion(nums, ++index);
+        ans.pop_back();
+        subsetReccursion(nums, index);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        
+        subsetReccursion(nums, 0);
+        return res;
+    }
+};
