ebaba3841e17717bfc24fa72aa37011b3909e262
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-10-22 21:08:13 +0530

    Time: 0 ms (100%), Space: 9 MB (6.71%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -5,13 +5,10 @@ public:
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
 
-        int count = 0, index = 0;
+        int count = 0;
         
         for(int i = 1; i < nums.size(); i++)
-            if(nums[i] < nums[i - 1]){
-                count++;
-                index=i;
-            }
+            if(nums[i] < nums[i - 1]) count++;
         
         if(count>1) return false;
         if(count == 1) if(nums[nums.size() - 1] > nums[0]) return false;
