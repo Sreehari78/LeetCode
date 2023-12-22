8e90b58aac09a33792b5e93e357d15d24e002b7e
Sreehari S <51209834+Sreehari78@users.noreply.github.com>
2023-12-22 09:40:48 +0530

    Time: 3 ms (50.89%), Space: 6.7 MB (62.95%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -10,8 +10,8 @@ public:
         int zeros = 0, ones = count(s.begin(), s.end(), '1'), score = 0, count = 0, n = s.length();
 
         for(int i = 0; i < n - 1; i++) {
-            if(s[i] == 0)zeros++;
-            else ones--;
+            zeros += (s[i] == '0');
+            ones -= (s[i] == '1');
             score = max(score, zeros + ones);
         }
         return score;
