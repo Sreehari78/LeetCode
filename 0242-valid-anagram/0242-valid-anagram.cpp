d4538f468a62cdf7426464179e6c00854f7e211d
Sreehari S <shraxx7@gmail.com>
2023-12-24 18:10:42 +0530

    Time: 3 ms (97.1%), Space: 7.7 MB (78.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
+    Solution() {
         ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);  
-        cout.tie(nullptr);  
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+    
+    bool isAnagram(string s, string t) {
+        unordered_map <char, int> mp;
 
-        unordered_map<char, int> mp1, mp2;
-        for(int i = 0; i < s.length() || i < t.length(); i++) {
-            if(i < s.length()) mp1[s[i]]++;
-            if(i < t.length()) mp2[t[i]]++;
-        }
-
-        if(mp1 == mp2) return true;
-        return false;
+        for(auto it: s) mp[it]++;
+        for(auto it: t) mp[it]--;
+        for(auto it: mp) if(it.second != 0) return false;
+        return true;
     }
 };
