38d227d6cad99f16fa3c5b3a5e2d11a9c30f0128
Sreehari S <shraxx7@gmail.com>
2024-01-05 18:38:10 +0530

    Time: 2756 ms (5.02%), Space: 6.4 MB (15.98%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -6,14 +6,14 @@ public:
         cout.tie(NULL);
     }
 
-    int divide(int dividend, int divisor) {
+    int divide(long dividend, long divisor) {
         if(dividend == INT_MIN && divisor == -1) return INT_MAX;
         if(dividend == INT_MIN && divisor == 1) return INT_MIN;
         if(divisor == 1) return dividend;
 
-        int count = 0;
+        long count = 0;
         bool flag1 = false, flag2 = false;
-        
+
         if(dividend < 0) flag1 = true;
         if(divisor < 0) flag2 = true;
 
@@ -26,6 +26,6 @@ public:
         }
 
         if(flag1 == flag2) return count;
-        else return count * -1;
+        else return -count;
     }
 };
