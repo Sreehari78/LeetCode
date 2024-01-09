9753849e608d6764bd8503c9d651f745d6dfc02f
Sreehari S <shraxx7@gmail.com>
2024-01-09 17:51:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -7,36 +7,35 @@ public:
     }
 
     int firstMissingPositive(vector<int>& nums) {
-        // vector<bool> v(100000, false);
-        // int maxi = INT_MIN;
+        vector<bool> v(100000, false);
 
-        // for (auto it : nums) {
-        //     if (it > 0 && it <= 100000)
-        //         v[it] = true;
-        //     maxi = max(it, maxi);
-        // }
-
-        // for (int i = 1; i <= maxi; i++)
-        //     if (!v[i])
-        //         return i;
-
-        // if(maxi < 0) return 1;
-        // return ++maxi;
-        int id;
-        for (int i = 0; i < nums.size(); i++)
-            if (nums[i] <= 0 || nums[i] > nums.size())
-                nums[i] = nums.size() + 1;
-
-        for (int i = 0; i < nums.size(); i++) {
-            id = abs(nums[i]);
-            if(id > nums.size()) continue;
-            id--;
-            if(nums[id] > 0) nums[id] = -nums[id];
+        for (auto it : nums) {
+            if (it > 0 && it <= 100000)
+                v[it] = true;
+            // maxi = max(it, maxi);
         }
 
-        for(int i = 0; i < nums.size(); i++)
-            if(nums[i] > 0) return i + 1;
+        for (int i = 1; i <= nums.size(); i++)
+            if (!v[i])
+                return i;
 
-        return nums.size() + 1;
+        if(maxi < 0) return 1;
+        return ++maxi;
+        // int id;
+        // for (int i = 0; i < nums.size(); i++)
+        //     if (nums[i] <= 0 || nums[i] > nums.size())
+        //         nums[i] = nums.size() + 1;
+
+        // for (int i = 0; i < nums.size(); i++) {
+        //     id = abs(nums[i]);
+        //     if(id > nums.size()) continue;
+        //     id--;
+        //     if(nums[id] > 0) nums[id] = -nums[id];
+        // }
+
+        // for(int i = 0; i < nums.size(); i++)
+        //     if(nums[i] > 0) return i + 1;
+
+        // return nums.size() + 1;
     }
 };
