6f935b03d1f01aa402c99970347d978df6a7e012
Sreehari S <shraxx7@gmail.com>
2024-01-09 15:56:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -5,17 +5,34 @@ public:
         cin.tie(NULL);
         cout.tie(NULL);
     }
-    
+
     int firstMissingPositive(vector<int>& nums) {
-        vector<bool> v(100000, false);
-        for (auto it : nums)
-            if (it > 0 && it <= 100000)
-                v[it] = true;
+        // vector<bool> v(100000, false);
+        // int maxi = INT_MIN;
 
-        for (int i = 1; i <= 100000; i++)
-            if (!v[i])
-                return i;
+        // for (auto it : nums) {
+        //     if (it > 0 && it <= 100000)
+        //         v[it] = true;
+        //     maxi = max(it, maxi);
+        // }
 
-        return 100001;
+        // for (int i = 1; i <= maxi; i++)
+        //     if (!v[i])
+        //         return i;
+
+        // if(maxi < 0) return 1;
+        // return ++maxi;
+
+        for(int i = 0; i < nums.size(); i++)
+            if (nums[i] < 1)
+                nums[i] == 0;
+            
+        for(int i = 0; i < nums.size(); i++)
+            if(nums[i] > 0 && nums[i] < nums.size()) nums[nums[i]] *= -1;
+        
+        for(int i = 1; i < nums.size(); i++)
+            if(nums[i] > 0) return i;
+
+         return nums.size() + 1;
     }
 };
