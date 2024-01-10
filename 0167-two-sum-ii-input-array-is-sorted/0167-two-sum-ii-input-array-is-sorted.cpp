45787422202649fbfb84fa217ad9529a73307d58
Sreehari S <shraxx7@gmail.com>
2024-01-10 07:32:28 +0530

    Time: 9 ms (65.74%), Space: 15.9 MB (59.25%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,11 +1,5 @@
 class Solution {
 public:
-    Solution() {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-    }
-
     vector<int> twoSum(vector<int>& numbers, int target) {
         int i = 0, j = numbers.size() - 1;
         while(i < j) {
