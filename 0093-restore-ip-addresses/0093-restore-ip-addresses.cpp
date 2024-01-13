004dececf52eb5a03471ebf7489b9cfa6b3b5a50
Sreehari S <shraxx7@gmail.com>
2024-01-13 12:55:30 +0530

    Time: 0 ms (100%), Space: 6.8 MB (94.14%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <string>
+
+class Solution {
+private:
+    vector<string> result;
+
+public:
+    void recursiveMethod(int index, const string& s, int dots, const string& ip) {
+        if (index == s.length() && dots == 4) {
+            result.push_back(ip);
+            return;
+        }
+
+        if (dots >= 4) return;
+
+        for (int i = 1; i <= 3 && index + i <= s.length(); i++) {
+            string ans = s.substr(index, i);
+            if ((ans[0] == '0' && ans.length() > 1) || (i == 3 && stoi(ans) >= 256)) {
+                // Skip substrings starting with '0' and substrings greater than or equal to 256
+                continue;
+            }
+
+            string newIp = ip.empty() ? ans : ip + '.' + ans;
+            recursiveMethod(index + i, s, dots + 1, newIp);
+        }
+    }
+
+    vector<string> restoreIpAddresses(const string& s) {
+        if (s.length() < 4 || s.length() > 12) return {};
+
+        recursiveMethod(0, s, 0, "");
+        return result;
+    }
+};
