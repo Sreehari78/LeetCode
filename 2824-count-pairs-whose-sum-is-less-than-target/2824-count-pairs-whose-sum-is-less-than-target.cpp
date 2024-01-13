1c09c1382e57e57d7a0cfcf5312d55976586dc42
Sreehari S <shraxx7@gmail.com>
2024-01-13 21:36:40 +0530

    Time: 3 ms (89.37%), Space: 20.9 MB (33.84%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
@@ -5,9 +5,8 @@ public:
         cin.tie(NULL);
         cout.tie(NULL);
     }
-    
-    int countPairs(vector<int>& nums, int target) {
 
+    int countPairs(vector<int>& nums, int target) {
         int count = 0;
         for(int i = 0; i < nums.size(); i++)
             for(int j = i + 1; j < nums.size(); j++)
