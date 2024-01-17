55452ab5a649e6e55912f665633036fdfb08be81
Sreehari S <shraxx7@gmail.com>
2024-01-17 09:43:51 +0530

    Time: 0 ms (100%), Space: 6.4 MB (77.23%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,11 +1,17 @@
-int speedUp = [] { ios::sync_with_stdio(0); cin.tie(0); return 0; }();
+// int speedUp = [] { ios::sync_with_stdio(0); cin.tie(0); return 0; }();
 class Solution {
 public:
+    void reccursiveMethod(int n, int k, string &ans, int fact, vector<int> &numbers) {
+        ans += to_string(numbers[k / fact]);
+        numbers.erase(numbers.begin() + k / fact);
+        if(numbers.size() == 0) return;
+        reccursiveMethod(n, k % fact, ans, fact / numbers.size(), numbers);
+    }
+
     string getPermutation(int n, int k) {
         vector<int> numbers;
         int fact = 1;
-        k--;
-        string ans;
+        string ans = "";
 
         for(int i = 1; i < n; i++) {
             fact *= i;
@@ -13,14 +19,16 @@ public:
         }
 
         numbers.push_back(n);
+        reccursiveMethod(n, k - 1, ans, fact, numbers);
 
-        while(true) {
-            ans += to_string(numbers[k / fact]);
-            numbers.erase(numbers.begin() + k / fact);
-            if(numbers.size() == 0) break;
-            k %= fact;
-            fact /= numbers.size();
-        }
+        // k--;
+        // while(true) {
+        //     ans += to_string(numbers[k / fact]);
+        //     numbers.erase(numbers.begin() + k / fact);
+        //     if(numbers.size() == 0) break;
+        //     k %= fact;
+        //     fact /= numbers.size();
+        // }
 
         return ans;
     }
