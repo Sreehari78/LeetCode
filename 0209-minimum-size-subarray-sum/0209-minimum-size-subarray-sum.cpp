f6da4f6b0251341f1a4cdcb039bc461db6582276
Sreehari S <shraxx7@gmail.com>
2024-01-18 08:18:02 +0530

    Time: 17 ms (98.61%), Space: 28.6 MB (56.38%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        
         int left = 0, right = 0, mini = nums.size(), sum = 0;
         bool flag = false;
 
