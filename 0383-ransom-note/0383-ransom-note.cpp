8952d6350cb24c9f62cffedb9edbe7bdb374e5be
Sreehari S <shraxx7@gmail.com>
2024-01-19 18:08:27 +0530

    Time: 7 ms (96.83%), Space: 11.2 MB (7.32%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,14 +1,11 @@
 int speedUp = [] { ios::sync_with_stdio(0); cin.tie(0); return 0; }();
+
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        if(ransomNote.length() > magazine.length()) return false;
-
         unordered_map<char, int> mp;
         for(int i = 0; i < magazine.size(); i++) mp[magazine[i]]++;
-
         for(int i = 0; i < ransomNote.size(); i++) if(!mp[ransomNote[i]]--) return false; 
-
         return true;
     }
 };
