2c06c2e35ffe637f74a1d22f5fb21b8f2cd20ed3
Sreehari S <shraxx7@gmail.com>
2024-01-22 07:31:31 +0530

    Time: 45 ms (37.48%), Space: 34.4 MB (8.31%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int repeated = 0, missing = (nums.size()*(nums.size() + 1)) / 2;
+        unordered_map<int, int> mp;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
+            if(mp[nums[i]] > 1) {
+                repeated = nums[i];
+                continue;
+            }
+            missing -= nums[i];
+        }
+
+        return {repeated, missing};
+    }
+};
