765fa95437e711a10a643acd12df44bc082febe6
Sreehari S <shraxx7@gmail.com>
2024-01-26 17:23:49 +0530

    Time: 0 ms (100%), Space: 7.4 MB (5.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -6,6 +6,9 @@ public:
         std::cout.tie(nullptr);
 
         vector<int> dp(n + 1, 0);
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        
         dp[0] = 1, dp[1] = 1;
         for(int i = 2; i <= n; i++) dp[i] = dp[i - 1] + dp[i - 2];
 
