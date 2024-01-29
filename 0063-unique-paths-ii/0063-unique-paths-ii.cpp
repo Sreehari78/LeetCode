de660de35079ca690af238370ae91c8939341ff1
Sreehari S <shraxx7@gmail.com>
2024-01-29 20:10:55 +0530

    Time: 6 ms (17.12%), Space: 10.3 MB (8.85%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -16,17 +16,21 @@ public:
         int n = obstacleGrid.size(), m = obstacleGrid[0].size() ;
         if(obstacleGrid[n - 1][m - 1] == 1) return 0;
 
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        for(int  i = 0; i < n; i++)
+        vector<int> prev(m, 0);
+        for(int  i = 0; i < n; i++) {
+            vector<int> curr(m, 0);
+
             for(int  j = 0; j < m; j++)
-                if(i == 0 && j == 0 && obstacleGrid[0][0] == 0) dp[0][0] = 1;
+                if(i == 0 && j == 0 && obstacleGrid[0][0] == 0) curr[0] = 1;
                 else {
                     int result = 0;
-                    if(i - 1 >= 0 && obstacleGrid[i - 1][j] != 1) result += dp[i - 1][j];
-                    if(j - 1 >= 0 && obstacleGrid[i][j - 1] != 1) result += dp[i][j - 1];
-                    dp[i][j] = result;
+                    if(i - 1 >= 0 && obstacleGrid[i - 1][j] != 1) result += prev[j];
+                    if(j - 1 >= 0 && obstacleGrid[i][j - 1] != 1) result += curr[j - 1];
+                    curr[j] = result;
                 }
+                prev = curr;
+        }
 
-        return dp[n - 1][m - 1];
+        return prev[m - 1];
     }
 };
