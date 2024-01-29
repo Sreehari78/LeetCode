fa6d321d7eee8505ed499ecfefded6de6dae93f5
Sreehari S <shraxx7@gmail.com>
2024-01-29 20:05:44 +0530

    Time: 0 ms (100%), Space: 10.2 MB (13.74%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -17,7 +17,16 @@ public:
         if(obstacleGrid[n - 1][m - 1] == 1) return 0;
 
         vector<vector<int>> dp(n, vector<int>(m, -1));
+        for(int  i = 0; i < n; i++)
+            for(int  j = 0; j < m; j++)
+                if(i == 0 && j == 0 && obstacleGrid[0][0] == 0) dp[0][0] = 1;
+                else {
+                    int result = 0;
+                    if(i - 1 >= 0 && obstacleGrid[i - 1][j] != 1) result += dp[i - 1][j];
+                    if(j - 1 >= 0 && obstacleGrid[i][j - 1] != 1) result += dp[i][j - 1];
+                    dp[i][j] = result;
+                }
 
-        return memo(obstacleGrid, n - 1, m - 1, dp);
+        return dp[n - 1][m - 1];
     }
 };
