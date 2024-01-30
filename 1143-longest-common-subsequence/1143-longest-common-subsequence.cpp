40a1011449a724b8cc12a1c21c264bdcec74f3df
Sreehari S <shraxx7@gmail.com>
2024-01-30 21:57:57 +0530

    Time: 16 ms (83.2%), Space: 25.8 MB (11.67%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -5,6 +5,7 @@ static const auto _ = []() {
   std::cin.tie(nullptr);
   return nullptr;
 }();
+
 class Solution {
 public:
 
@@ -13,15 +14,10 @@ public:
         int n = text2.length();
         vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                if (text1[i - 1] == text2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
 
         return dp[m][n];
     }
