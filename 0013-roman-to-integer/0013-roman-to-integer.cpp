ee2966b0094bae46cede32c320a2e133ddae8354
Sreehari S <shraxx7@gmail.com>
2024-02-03 08:19:47 +0530

    Time: 0 ms (100%), Space: 9.5 MB (18.68%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -9,48 +9,29 @@ public:
             switch(symbol)
             {
                 case 'I':
-                    if((i+1)<s.length() && (s[i+1] == 'V' || s[i+1] == 'X'))
-                    {
-                        number -= 1;
-                    }
-                    else
-                    {
-                        number += 1;
-                    }
+                    if((i+1)<s.length() && (s[i+1] == 'V' || s[i+1] == 'X')) number -= 1;                    
+                    else number += 1;                    
                     break;
                 case 'V':
                     number += 5;
                     break;
                 case 'X':
-                    if((i+1)<s.length() && (s[i+1] == 'L' || s[i+1] == 'C'))
-                    {
-                        number -= 10;
-                    }
-                    else
-                    {
-                        number += 10;
-                    }
+                    if((i+1)<s.length() && (s[i+1] == 'L' || s[i+1] == 'C')) number -= 10;
+                    else number += 10;
                     break;
                 case 'L':
                     number += 50;
                     break;
                 case 'C':
-                    if((i+1)<s.length() && (s[i+1] == 'D' || s[i+1] == 'M'))
-                    {
-                        number -= 100;
-                    }
-                    else
-                    {
-                        number += 100;
-                    }
+                    if((i+1)<s.length() && (s[i+1] == 'D' || s[i+1] == 'M')) number -= 100;
+                    else number += 100;
                     break;
                 case 'D':
                     number += 500;
                     break;
                 case 'M':
                     number += 1000;
-                    break;
-                        
+                    break;       
             }
         }
 
