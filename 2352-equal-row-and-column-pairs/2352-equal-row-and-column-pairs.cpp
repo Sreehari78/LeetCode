0148f4cbbde35120755494dfa8a4fba20f3eb5d4
Sreehari S <shraxx7@gmail.com>
2024-02-04 17:23:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,27 +1,7 @@
-struct VectorHasher {
-    int operator()(const std::vector<int> &V) const {
-        int hash = V.size();
-        for(auto &i : V) {
-        hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
-        }
-        return hash;
-    }
-};
-
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
-
-static const int FAST__ = [](){
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return 0;
-}();
-
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        unordered_map<vector<int>,int, VectorHasher> mp;
+        map<vector<int>,int, VectorHasher> mp;
         int result = 0;
 
         for(int i = 0; i < grid.size(); i++) mp[grid[i]]++;
