6ba6a7baedb89ce9150528ac834aad68d20ace52
Sreehari S <shraxx7@gmail.com>
2024-02-04 18:06:42 +0530

    Time: 42 ms (99.77%), Space: 28.5 MB (10.71%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,4 +1,10 @@
-int speedUp = [] { ios::sync_with_stdio(0); cin.tie(0); return 0; }();
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
 class Solution {
 public:
     string removeStars(string s) {
