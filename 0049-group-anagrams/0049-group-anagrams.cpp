ef35eef89bd722501edf029e94a175c1a6d649e6
Sreehari S <shraxx7@gmail.com>
2024-02-07 07:00:38 +0530

    Time: 14 ms (99.64%), Space: 23.5 MB (31.59%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        
         std::unordered_map<string, vector<string>> mapping;
         for (const auto& str : strs) {
             string new_str = str;
