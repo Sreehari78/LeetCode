70275dc1e2660a477be5c7ed4a33964962a2edb3
Sreehari S <shraxx7@gmail.com>
2024-02-07 07:00:11 +0530

    Time: 26 ms (79.6%), Space: 23.3 MB (36.5%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
-    string getSignature(const string& s) {
-        vector<int> count(26, 0);
-        for (char c : s) {
-            count[c - 'a']++;
-        }
-
-        stringstream ss;
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                ss << (char)('a' + i) << count[i];
-            }
-        }
-        return ss.str();
-    }
-
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> result;
-        unordered_map<string, vector<string>> groups;
-
-        for (const string& s : strs) {
-            groups[getSignature(s)].push_back(s);
+        std::unordered_map<string, vector<string>> mapping;
+        for (const auto& str : strs) {
+            string new_str = str;
+            sort(new_str.begin(), new_str.end());
+            mapping[new_str].push_back(str);
         }
-
-        for (const auto& entry : groups) {
-            result.push_back(entry.second);
+        vector<vector<string>> ans;
+        for (const auto& entry : mapping) {
+            ans.push_back(entry.second);
         }
-
-        return result;
+        return ans;
     }
 };
