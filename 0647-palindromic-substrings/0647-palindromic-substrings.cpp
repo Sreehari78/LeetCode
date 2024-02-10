0bb0a009a38982df182cd95d6c4e97368638f200
Sreehari S <shraxx7@gmail.com>
2024-02-10 19:14:25 +0530

    Time: 4 ms (76.2%), Space: 7.4 MB (78.14%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int countSubstrings(string s) {
         
-        vector<string> result;
+        int result = 0;
 
         for(int i = 0; i < s.length(); i++) {
             int  left = i, right = i;
             while(left > -1 && right < s.length()) 
                 if(s[left] == s[right]) {
-                    result.push_back(s.substr(left, right - left + 1));
+                    result++;
                     left--;
                     right++;
                 }
@@ -19,12 +19,12 @@ public:
             int  left = i, right = i+1;
             while(left > -1 && right < s.length()) 
                 if(s[left] == s[right]) {
-                    result.push_back(s.substr(left, right - left + 1));
+                    result++;
                     left--;
                     right++;
                 }
                 else break;
        }
-       return result.size();
+       return result;
     }
 };
