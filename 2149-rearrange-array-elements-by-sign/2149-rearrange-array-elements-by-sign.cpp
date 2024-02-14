3167f489e30bbd2f0db006ad2a38b577573d783c
Sreehari S <shraxx7@gmail.com>
2024-02-14 07:31:01 +0530

    Time: 93 ms (99.63%), Space: 125.9 MB (66.54%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -5,17 +5,32 @@ public:
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
         
-        vector<int> positive, negative;
-        int p = 0, n = 0;
+        // vector<int> positive, negative;
+        // int p = 0, n = 0;
 
-        for(int it: nums) 
-            if(it > 0) positive.push_back(it); 
-            else negative.push_back(it);
+        // for(int it: nums) 
+        //     if(it > 0) positive.push_back(it); 
+        //     else negative.push_back(it);
 
-        for(int i = 0; i < nums.size(); i++) 
-            if(i % 2 == 0) nums[i] = positive[p++]; 
-            else nums[i] = negative[n++];
+        // for(int i = 0; i < nums.size(); i++) 
+        //     if(i % 2 == 0) nums[i] = positive[p++]; 
+        //     else nums[i] = negative[n++];
 
-        return nums;
+        // return nums;
+
+        int a = 0, b = 1, n = nums.size();
+        vector<int> ans(n);
+
+        for(int i = 0; i < n; i++) 
+            if(nums[i] > 0) {
+                ans[a] = nums[i];
+                a += 2;
+            }
+            else {
+                ans[b] = nums[i];
+                b += 2;
+            }
+        
+        return ans;
     }
 };
