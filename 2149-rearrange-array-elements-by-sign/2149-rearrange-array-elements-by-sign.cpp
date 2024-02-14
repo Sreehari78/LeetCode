51b4f00c00d8df3ad95e2e1d017fca200240efcc
Sreehari S <shraxx7@gmail.com>
2024-02-14 07:24:23 +0530

    Time: 93 ms (99.63%), Space: 136.7 MB (23.92%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         
         vector<int> positive, negative;
         int p = 0, n = 0;
