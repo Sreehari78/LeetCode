3caff372ea07b3495f24e436c5be23784ac3b2df
Sreehari S <shraxx7@gmail.com>
2024-02-15 08:19:08 +0530

    Time: 65 ms (99.99%), Space: 83.1 MB (36.5%) - LeetHub

--- 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.cpp
+++ 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.cpp
@@ -4,13 +4,13 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
-        
+
         sort(nums.begin(), nums.end());
         long long n = nums.size(), perimeter = 0;
 
         for(int it: nums) perimeter += it;
 
-        for(int i = n - 1; i >= 2; i--) {
+        for(int i = n - 1; i > -1; i--) {
             perimeter -= nums[i];
             if(perimeter > nums[i]) return perimeter + nums[i];
         }
