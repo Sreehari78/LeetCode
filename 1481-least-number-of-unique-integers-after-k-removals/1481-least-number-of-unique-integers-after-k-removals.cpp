eaa012e359afa69f5164c6a391c62e5aac7ca4cc
Sreehari S <shraxx7@gmail.com>
2024-02-16 09:03:19 +0530

    Time: 71 ms (99.77%), Space: 65.2 MB (73.35%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -1,30 +1,31 @@
 static int speedup = []() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(0);
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(0);
+    return 0;
 }();
 class Solution {
 public:
-    int findLeastNumOfUniqueInts(vector<int>& arr, int k) 
-    {
-        unordered_map<int,int>mp; 
-        for(int it : arr) mp[it]++;
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        unordered_map<int, int> mp;
+        for (int it : arr)
+            mp[it]++;
 
-        vector<int>freq;
-        for(auto it : mp) freq.push_back(it.second);
+        vector<int> freq;
+        for (auto it : mp)
+            freq.push_back(it.second);
 
-        sort(freq.begin(),freq.end());
+        sort(freq.begin(), freq.end());
 
         int i = 0;
-        for(; i < freq.size(); i++)
-        {
-            k-= freq[i];               
-            if(k <= 0) break; 
-            
-        }   
+        for (; i < freq.size(); i++) {
+            k -= freq[i];
+            if (k <= 0)
+                break;
+        }
 
-        if(k == 0) return(freq.size() - i - 1); 
-        return(freq.size() - i);
+        if (k == 0)
+            return (freq.size() - i - 1);
+        return (freq.size() - i);
     }
 };
