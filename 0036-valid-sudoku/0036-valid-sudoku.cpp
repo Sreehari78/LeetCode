011322be1b309497f6022198adf535344ce67092
Sreehari S <shraxx7@gmail.com>
2024-02-16 22:17:44 +0530

    Time: 16 ms (67.07%), Space: 24.7 MB (9.37%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:      
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<vector<char>> transpose(9, vector<char>(9));
+        vector<unordered_set<char>> rows(9), cols(9), boxes(9);
+
+        
+        for(int i = 0; i < 9; i++) for(int j = 0; j < 9; j++) transpose[i][j] = board[j][i]; 
+
+        //rows
+        for(vector<char> it: board) 
+            for(int j = 0; j < 9; j++) 
+                for(int k = j + 1; k < 9; k++) if(it[k] != '.' && it[j] == it[k]) return false;
+        
+        //columns
+        for(vector<char> it: transpose) 
+            for(int j = 0; j < 9; j++) 
+                for(int k = j + 1; k < 9; k++) if(it[k] != '.' && it[j] == it[k]) return false;
+        
+        //3x3 grids
+        for(int i = 0; i < 9; i++)
+            for(int j = 0; j < 9; j++) {
+                char cell = board[i][j];
+                if(cell == '.') continue;
+
+                int boxIndex = (i / 3) * 3 + j / 3;
+                if (boxes[boxIndex].count(cell)) return false;
+                boxes[boxIndex].insert(cell);
+            }
+
+        return true;
+    }
+};
