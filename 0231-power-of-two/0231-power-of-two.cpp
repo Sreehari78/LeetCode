76d8348d13a105ee12bf875182223c996a982d9a
Sreehari S <shraxx7@gmail.com>
2024-02-19 19:20:19 +0530

    Time: 4 ms (22.67%), Space: 7.4 MB (19.05%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        
         if(n == 0) return false;
         while(n % 2 == 0) n /= 2;
         return n == 1;
