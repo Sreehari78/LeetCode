a71abefc6ca0469afcce06943e4e252e5041ae70
Sreehari S <shraxx7@gmail.com>
2024-02-19 08:19:45 +0530

    Time: 0 ms (100%), Space: 7.1 MB (12.34%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.cpp
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -4,8 +4,8 @@ public:
         if(n == 1) return false;
 
         int result = 0;
-        for(int i = 1; i <= n; i++)
-            if(n % i == 0) result++;
+        for(int i = 1; i <= n; i++) if(n % i == 0) result++;
+        
         
         return result == 3;
     }
