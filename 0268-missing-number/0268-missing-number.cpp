d64f9f7b87ba6be0db22619e77730464302325d6
Sreehari S <shraxx7@gmail.com>
2024-02-20 07:54:35 +0530

    Time: 4 ms (99.12%), Space: 20.5 MB (16.85%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -4,7 +4,7 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
-        
+
         int sum = (nums.size() * (nums.size() + 1)) / 2;
         return sum - accumulate(nums.begin(), nums.end(), 0);
     }
