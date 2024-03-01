ec9ee928c4f2fb442c74952b816acc554025ae29
Sreehari S <shraxx7@gmail.com>
2024-03-02 00:39:22 +0530

    Time: 3 ms (77.44%), Space: 8.6 MB (29.81%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
         
         int ones = 0;
         string result = "";
