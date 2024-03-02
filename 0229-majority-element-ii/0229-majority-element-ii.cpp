7655c45d4b0960e8645a1de5ecbe04888d3a9949
Sreehari S <shraxx7@gmail.com>
2024-03-03 02:51:28 +0530

    Time: 0 ms (100%), Space: 19.7 MB (5.43%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -4,15 +4,18 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
-        
+
         unordered_map<int, int> mp;
         vector<int> result;
         int n = nums.size();
 
-        for(int i = 0; i < n; i++) mp[nums[i]]++;
+        for (int i = 0; i < n; i++)
+            mp[nums[i]]++;
 
-        for(auto it: mp) if(it.second > n / 3) result.push_back(it.first);
+        for (auto it : mp)
+            if (it.second > n / 3)
+                result.push_back(it.first);
+                
         return result;
-
     }
 };
