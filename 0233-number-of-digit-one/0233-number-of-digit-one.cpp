a72e6038a971c3bc63631e2edd827ddabd525cf8
Sreehari S <shraxx7@gmail.com>
2024-03-02 21:24:19 +0530

    Time: 2 ms (51.97%), Space: 7 MB (58.53%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.cpp
+++ 0233-number-of-digit-one/0233-number-of-digit-one.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int countDigitOne(int n) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        
         int count = 0;
         for (long long i = 1; i <= n; i *= 10) {
             long long divider = i * 10;
