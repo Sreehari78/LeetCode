db399b2771e45c467214765e707e5b951b0fac5b
Sreehari S <shraxx7@gmail.com>
2024-03-02 11:07:18 +0530

    Time: 11 ms (98.91%), Space: 28.5 MB (43.69%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+
         int n = nums.size();
         int left = 0, right = n - 1;
         vector<int> sorted(n, -1);
