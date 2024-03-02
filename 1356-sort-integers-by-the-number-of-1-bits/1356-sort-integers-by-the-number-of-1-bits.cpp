5a227b09c6d55f01defbeb10287d79a0d3d31273
Sreehari S <shraxx7@gmail.com>
2024-03-03 03:04:24 +0530

    Time: 10 ms (36.49%), Space: 12.5 MB (66.24%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
-    static bool compareByValue(const pair<int, int>& a, const pair<int, int>& b) {
-        if (a.second != b.second)
-            return a.second < b.second;
-        return a.first < b.first;
+    static bool comp(int a, int b) {
+        int countA = __builtin_popcount(a);
+        int countB = __builtin_popcount(b);
+
+        if (countA == countB) return a < b;
+        else return countA < countB;
     }
 
     vector<int> sortByBits(vector<int>& arr) {
-        int n = arr.size();
-        unordered_map<int, int> mp;
-        vector<pair<int, int>> pairs;
-
-        for (int i = 0; i < n; i++) {
-            int x = arr[i];
-            int count = 0;
-            while (x) {
-                count += x & 1;
-                x >>= 1;
-            }
-            mp[arr[i]] = count;
-            pairs.push_back({arr[i], count});
-        }
-        
-        sort(pairs.begin(), pairs.end(), compareByValue);
-        
-        vector<int> result;
-        for(auto it: pairs) 
-            result.push_back(it.first);
-        return result;
+        sort(arr.begin(), arr.end(), comp);
+        return arr;
     }
 };
