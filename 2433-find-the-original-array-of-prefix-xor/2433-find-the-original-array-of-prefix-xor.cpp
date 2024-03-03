5cdfe4770e85affb74bb9ed714994246f2c0352b
Sreehari S <shraxx7@gmail.com>
2024-03-03 10:13:11 +0530

    Time: 46 ms (99.36%), Space: 78.2 MB (77.5%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -6,9 +6,9 @@ public:
         cout.tie(NULL);
 
         int n = pref.size();
-        vector<int> result;
-        result.push_back(pref[0]);
-        for(int i = 1; i < n; i++) result.push_back(pref[i - 1] ^ pref[i]);
+        vector<int> result(n, -1);
+        result[0] = pref[0];
+        for(int i = 1; i < n; i++) result[i] = pref[i - 1] ^ pref[i];
 
         return result;
     }
