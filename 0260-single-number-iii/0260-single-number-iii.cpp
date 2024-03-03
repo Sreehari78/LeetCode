5d67bb06652c87d6fc484d704305f648fdfdbcf4
Sreehari S <shraxx7@gmail.com>
2024-03-03 10:20:50 +0530

    Time: 12 ms (21.68%), Space: 14.4 MB (9.87%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+
         unordered_map <int, int> mp;
         vector<int> result(2, 0);
         int i = 0;
