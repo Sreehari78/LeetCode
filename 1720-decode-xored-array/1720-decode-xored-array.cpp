0016bea4d5297bc3e6010e8959ad3f98fdd8e610
Sreehari S <shraxx7@gmail.com>
2024-03-03 10:30:12 +0530

    Time: 25 ms (26.68%), Space: 28.4 MB (31.19%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     vector<int> decode(vector<int>& encoded, int first) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-
         vector<int> result;
         result.push_back(first);
 
-        for(int i = 0; i < encoded.size(); i++) result.push_back(result[i] ^ encoded[i]);
+        for (int i = 0; i < encoded.size(); i++)
+            result.push_back(result[i] ^ encoded[i]);
+            
         return result;
     }
 };
