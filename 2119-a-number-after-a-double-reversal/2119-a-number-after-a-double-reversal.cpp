1f3a557a88c7c5059f8d4cafc36d0fdbc905d2a1
Sreehari S <shraxx7@gmail.com>
2024-03-04 17:45:08 +0530

    Time: 3 ms (24.94%), Space: 7.1 MB (13.29%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -1,23 +1,8 @@
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-        int org = num, rev = 0;
-        
-        while(org) {
-            rev *= 10;
-            rev += org % 10;
-            org /= 10;
-        }
-
-        org = 0;
-        while(rev) {
-            org *= 10;
-            org += rev % 10;
-            rev /= 10;
-            cout<< rev<<endl;
-        }
-        
-
-        return org == num;
+        if(num==0) return true;
+        if (num % 10 == 0) return false;
+        return true;
     }
 };
