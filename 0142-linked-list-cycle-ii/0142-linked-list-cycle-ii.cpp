8d148fa3fb9e5099b7d4cc36965f97d6c63de809
Sreehari S <shraxx7@gmail.com>
2024-03-06 21:36:58 +0530

    Time: 8 ms (50.77%), Space: 10.1 MB (36.17%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,6 +9,10 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        
         ListNode* hare = head;
         ListNode* tortise = head;
 
