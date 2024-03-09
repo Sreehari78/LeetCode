d436de35b61b553156bad5b9489ba6da909f280d
Sreehari S <shraxx7@gmail.com>
2024-03-09 07:02:08 +0530

    Time: 30 ms (99.33%), Space: 52.5 MB (86.75%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -5,13 +5,15 @@ public:
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
 
-        
         int left = 0, right = 0;
 
-        while(left < nums1.size() && right < nums2.size()) {
-            if(nums1[left] == nums2[right]) return nums2[right];
-            if(nums1[left] > nums2[right]) right++;
-            else left++;
+        while (left < nums1.size() && right < nums2.size()) {
+            if (nums1[left] == nums2[right])
+                return nums2[right];
+            if (nums1[left] > nums2[right])
+                right++;
+            else
+                left++;
         }
 
         return -1;
