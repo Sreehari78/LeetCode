34f40abcf87516a28866e0783f047aebad3b5b1e
Sreehari S <shraxx7@gmail.com>
2024-03-09 07:01:03 +0530

    Time: 30 ms (99.33%), Space: 52.6 MB (86.28%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        
         int left = 0, right = 0;
 
         while(left < nums1.size() && right < nums2.size()) {
