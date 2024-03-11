112d59c0fd0edf6516fe1e01229f73eccad2ce7a
Sreehari S <shraxx7@gmail.com>
2024-03-11 23:07:25 +0530

    Time: 4 ms (26.39%), Space: 7.9 MB (48.22%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -4,22 +4,17 @@ public:
         unordered_map<char, int> frequency;
         string result;
 
-        // Count the frequency of each character in s
         for (char ch : s)
             frequency[ch]++;
 
-        // Append characters in order according to their occurrence in s
-        for (char ch : order) {
+        for (char ch : order)
             if (frequency.find(ch) != frequency.end()) {
                 result.append(frequency[ch], ch);
-                frequency.erase(ch); // Remove the character from frequency map
+                frequency.erase(ch);
             }
-        }
 
-        // Append remaining characters that are not in the order
-        for (auto& entry : frequency) {
+        for (auto& entry : frequency)
             result.append(entry.second, entry.first);
-        }
 
         return result;
     }
