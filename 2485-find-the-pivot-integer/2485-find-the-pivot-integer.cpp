77995cd6dbfb31af393f90cf29bba41b30ca17c8
Sreehari S <shraxx7@gmail.com>
2024-03-13 08:28:47 +0530

    Time: 0 ms (100%), Space: 7.1 MB (73.82%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
-
         return sqrt(n * (n + 1) / 2) == int(sqrt(n * (n + 1) / 2)) ? sqrt(n * (n + 1) / 2) : -1;
     }
 };
