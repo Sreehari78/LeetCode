716156c48c212aeb389c4996e5206907e3a01a98
Sreehari S <shraxx7@gmail.com>
2024-03-13 07:34:35 +0530

    Time: 33 ms (11.78%), Space: 24.7 MB (5.18%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        if(n == 1) return 1;
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
 
-        unordered_map<int, pair<int,int>> mp;
+        if (n == 1)
+            return 1;
+
+        unordered_map<int, pair<int, int>> mp;
         int left = 1, right = n, sum = 0;
 
-        for(int x = 1; x <= n; x++) {
+        for (int x = 1; x <= n; x++) {
             sum += x;
             mp[x].first = sum;
         }
         sum = 0;
-        for(int x = n; x > 0; x--) {
+        for (int x = n; x > 0; x--) {
             sum += x;
-            mp[x].second =sum;
+            mp[x].second = sum;
         }
 
-        for(auto it: mp) if(it.second.first == it.second.second)return it.first;
+        for (auto it : mp)
+            if (it.second.first == it.second.second)
+                return it.first;
 
         return -1;
-                
-
     }
 };
