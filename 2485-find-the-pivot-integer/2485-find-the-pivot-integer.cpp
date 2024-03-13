67d545ee77d5bb3eae3ec830ead81c2057040ee2
Sreehari S <shraxx7@gmail.com>
2024-03-13 07:34:04 +0530

    Time: 35 ms (11.33%), Space: 24.4 MB (5.18%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -3,18 +3,20 @@ public:
     int pivotInteger(int n) {
         if(n == 1) return 1;
 
-        for(int x = 1; x < n; x++) {
-            int sum1 = 0, sum2 = 0;
+        unordered_map<int, pair<int,int>> mp;
+        int left = 1, right = n, sum = 0;
 
-            for(int i = 1; i <= n; i++) {
-                if(i <= x) sum1 += i;
-                if(i >= x) sum2 += i;
-            }
-            // cout<<sum1<<" "<<sum2<<endl;
-
-
-            if(sum1 == sum2) return x;
+        for(int x = 1; x <= n; x++) {
+            sum += x;
+            mp[x].first = sum;
         }
+        sum = 0;
+        for(int x = n; x > 0; x--) {
+            sum += x;
+            mp[x].second =sum;
+        }
+
+        for(auto it: mp) if(it.second.first == it.second.second)return it.first;
 
         return -1;
                 
