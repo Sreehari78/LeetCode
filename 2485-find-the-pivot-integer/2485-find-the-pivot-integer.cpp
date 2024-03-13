f19b906ba4809e0baa0bb8044195f6924843390a
Sreehari S <shraxx7@gmail.com>
2024-03-13 08:28:23 +0530

    Time: 3 ms (51.39%), Space: 7.3 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -5,21 +5,6 @@ public:
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
 
-        if (n == 1)
-            return 1;
-
-        int start = 1, end = n, sum = n * (n + 1) / 2;
-
-        while (start < end) {
-            int mid = (start + end) / 2, midSum = mid * (mid + 1) / 2;
-            if (midSum == sum - midSum + mid)
-                return mid;
-            else if (midSum < sum - midSum + mid)
-                start = mid + 1;
-            else
-                end = mid - 1;
-        }
-
-        return -1;
+        return sqrt(n * (n + 1) / 2) == int(sqrt(n * (n + 1) / 2)) ? sqrt(n * (n + 1) / 2) : -1;
     }
 };
