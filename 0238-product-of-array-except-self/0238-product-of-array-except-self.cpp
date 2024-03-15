0f97df44fc1849ccf10d70cb4659cc612db57b3b
Sreehari S <shraxx7@gmail.com>
2024-03-15 08:36:25 +0530

    Time: 21 ms (20.66%), Space: 27.4 MB (34.75%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,14 +4,11 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
-        
+
         vector<int> output;
-        int n = nums.size();
+        int right = 1, n = nums.size();
         output.push_back(1);
-        for (int i = 1; i <= n; i++) {
-            output.push_back(output[i - 1] * nums[i - 1]);
-        }
-        int right = 1;
+        for (int i = 1; i <= n; i++) output.push_back(output[i - 1] * nums[i - 1]);
         for (int i = n - 1; i >= 0; i--) {
             output[i] *= right;
             right *= nums[i];
