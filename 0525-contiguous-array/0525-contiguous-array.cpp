cc970d76932f5e8972a29daa488f4654bd494a0c
Sreehari S <shraxx7@gmail.com>
2024-03-16 15:41:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -1,3 +1,10 @@
+auto speedUp = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}()
+
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
