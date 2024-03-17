04b35fc06c65da53b47a915ba8a33a4b2ef16fd8
Sreehari S <shraxx7@gmail.com>
2024-03-17 23:48:09 +0530

    Time: 9 ms (76.06%), Space: 20.4 MB (82.99%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -9,11 +9,10 @@ public:
         while (i < intervals.size() && intervals[i][1] < newInterval[0])
             merged.push_back(intervals[i++]);
 
-        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
+        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) 
             newInterval = {min(newInterval[0], intervals[i][0]),
-                           max(newInterval[1], intervals[i][1])};
-            i++;
-        }
+                           max(newInterval[1], intervals[i++][1])};
+                           
         merged.push_back(newInterval);
 
         while (i < intervals.size())
