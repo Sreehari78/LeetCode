4b1b9a4cd3b5cf6d975b6c30682203ec161c1b21
Sreehari S <shraxx7@gmail.com>
2024-03-19 18:25:18 +0530

    Time: 34 ms (99.08%), Space: 37.8 MB (94.23%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-   int leastInterval(vector<char>& tasks, int n) {
+    int leastInterval(vector<char>& tasks, int n) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
         int freq[26] = {0};
-        for(char task : tasks){
+
+        for (char task : tasks)
             freq[task - 'A']++;
-        }
-        sort(begin(freq) , end(freq));
+
+        sort(begin(freq), end(freq));
         int chunk = freq[25] - 1;
         int idel = chunk * n;
 
-        for(int i=24; i>=0; i--){
-            idel -= min(chunk,freq[i]);
-        }
+        for (int i = 24; i >= 0; i--)
+            idel -= min(chunk, freq[i]);
 
         return idel < 0 ? tasks.size() : tasks.size() + idel;
     }
