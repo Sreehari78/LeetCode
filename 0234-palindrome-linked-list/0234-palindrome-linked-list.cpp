b94db6dcf2cb24d8885e71b2863c77fd1c580919
Sreehari S <shraxx7@gmail.com>
2024-03-22 20:53:26 +0530

    Time: 191 ms (21.63%), Space: 125.8 MB (27.95%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -8,31 +8,28 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
- int init = [] {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);  
-    ofstream out("user.out"); 
-    for (string s; getline(cin, s);) 
-        out << (equal(s.begin()+1, s.begin()+s.size()/2, s.rbegin()+1) ? "true\n" : "false\n");  
-    out.flush(); 
-    exit(0);  
-    return 0;
-}();
-
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int> v;
-        while(head) {
-            v.push_back(head -> val);
-            head = head -> next;
+        stack<int> st;
+        ListNode *temp=head;
+        while(temp!=NULL)
+        {
+            st.push(temp->val);
+            temp=temp->next;
         }
+        temp=head;
+while (temp != NULL) {
+    if (!st.empty() && st.top() == temp->val) {
+        st.pop(); // Remove the top element from the stack
+        temp = temp->next;
+    } else {
+        return false; // If elements don't match, it's not a palindrome
+    }
+}
+// If the loop completes without returning false, it's a palindrome
+return true;
+
         
-        int n = v.size();
-        for(int i = 0; i < n / 2; i++) {
-            if(v[i] != v[n - i - 1]) return false;
-        }
-        return true;
     }
 };
