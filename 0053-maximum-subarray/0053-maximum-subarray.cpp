2066cfcf325390427c1efb9b717348ea4538e063
Sreehari S <shraxx7@gmail.com>
2024-03-22 21:22:13 +0530

    Time: 36 ms (99.53%), Space: 72.7 MB (6.94%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+
         vector<int> dp(nums.size(), INT_MIN);
         dp[0] = nums[0];
+
         for (int i = 1; i < nums.size(); i++)
             dp[i] = max(nums[i], dp[i - 1] + nums[i]);
+
         int maxi = INT_MIN;
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++)
             if (dp[i] > maxi)
                 maxi = dp[i];
-        }
+                
         return maxi;
     }
 };
