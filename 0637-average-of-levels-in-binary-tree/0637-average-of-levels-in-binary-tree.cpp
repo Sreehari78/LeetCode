be437a40c200eea30bb9c248bce2c137941b5ab3
Sreehari S <shraxx7@gmail.com>
2024-03-24 08:58:56 +0530

    Time: 14 ms (33.76%), Space: 22.4 MB (43.93%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
@@ -6,7 +6,8 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
@@ -15,24 +16,27 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
-        
+
         vector<double> result;
         queue<TreeNode*> q;
 
-        if(!root) return {0};
+        if (!root)
+            return {0};
         q.push(root);
 
-        while(!q.empty()) {
+        while (!q.empty()) {
             int n = q.size();
             double avg = 0;
 
-            for(int i = 0; i < n; i++) {
+            for (int i = 0; i < n; i++) {
                 root = q.front();
                 q.pop();
 
-                if(root -> left) q.push(root -> left);
-                if(root -> right) q.push(root -> right);
-                avg += root -> val;
+                if (root->left)
+                    q.push(root->left);
+                if (root->right)
+                    q.push(root->right);
+                avg += root->val;
             }
 
             result.push_back(avg / n);
