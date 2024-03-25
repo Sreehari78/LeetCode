e69ccb64732cb1e484795a3c72873f77ce1f71c5
Sreehari S <shraxx7@gmail.com>
2024-03-25 08:43:43 +0530

    Time: 11 ms (98.55%), Space: 23.9 MB (68.3%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,14 +1,26 @@
-int speedUp = [] { ios::sync_with_stdio(0); cin.tie(0); return 0; }();
+int speedUp = [] {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}();
+
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        vector<int> H(nums.size() + 1, 0);
-        H[0] = 1;
-        for (auto &v: nums) {
-            H[v]++;
+        int repeat, missing;
+
+        for (int it : nums) {
+            int x = abs(it);
+            if (nums[x - 1] < 0)
+                repeat = abs(it);
+            else
+                nums[x - 1] *= -1;
         }
-        int d = max_element(H.begin(), H.end()) - H.begin();
-        int m = min_element(H.begin()+1, H.end()) - H.begin();
-        return {d, m};
+
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[i] > 0)
+                missing = i + 1;
+
+        return {repeat, missing};
     }
 };
