20c03bec8b4185c83000946a68e07831d08eb938
Sreehari S <shraxx7@gmail.com>
2024-03-30 22:16:47 +0530

    Time: 96 ms (76.57%), Space: 50 MB (24.85%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -3,20 +3,26 @@ private:
     int subarray_with_atmost_k(vector<int>& nums, int k) {
         unordered_map<int, int> map;
         int left = 0, right = 0, ans = 0;
+
         while (right < nums.size()) {
             map[nums[right]]++;
+
             while (map.size() > k) {
                 map[nums[left]]--;
+
                 if (map[nums[left]] == 0)
                     map.erase(nums[left]);
+
                 left++;
             }
+
             ans += right - left + 1;
             right++;
         }
+        
         return ans;
     }
-    
+
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
         std::ios_base::sync_with_stdio(false);
@@ -25,8 +31,7 @@ public:
 
         int sub_with_max_element_k = subarray_with_atmost_k(nums, k);
         int reduced_sub_with_max_k = subarray_with_atmost_k(nums, k - 1);
+        
         return (sub_with_max_element_k - reduced_sub_with_max_k);
     }
-
-    
 };
