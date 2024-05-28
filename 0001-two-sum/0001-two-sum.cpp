170312b1b6aa5eb21ac05463ef6fcdc9a345329a
Sreehari S <shraxx7@gmail.com>
2024-05-28 19:45:15 +0530

    Time: 3 ms (98.54%), Space: 14.1 MB (30%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
         unordered_map<int, int> mp;
         
         for(int i = 0; i < nums.size(); i++) {
