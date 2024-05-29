6a9fa27b07d6d4b10e3dee9c7a117d2ba211b091
Sreehari S <shraxx7@gmail.com>
2024-05-29 12:41:34 +0530

    Time: 0 ms (100%), Space: 7.4 MB (52.09%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     int reverse(int x) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
-        
-        long temp = 0, flag = 0;
-        
-        if(x < 0) flag = 1;
-        x = abs(x);
-
-        while(x > 0) {
-            temp = temp * 10 + x % 10;
-            x /= 10;
+        long res = 0;
+        while(x) {
+            res *= 10;
+            res += x % 10;
+            x = x / 10;
         }
 
-        if(temp > INT_MAX || temp < INT_MIN) return 0;
-
-        if(flag) temp *= -1;
-        x = temp;
-        return temp;
+        if(res < INT_MAX && res > INT_MIN) return res;
+        return 0;
     }
 };
