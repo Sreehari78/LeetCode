0ddb43d4c4031a44a089480121b76b100e0d4546
Sreehari S <shraxx7@gmail.com>
2024-05-29 12:47:24 +0530

    Time: 3 ms (93.51%), Space: 8.2 MB (78.96%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
+        if(x < 0) return false;
+        long res = 0, y = x;
         
-        if(x < 0) return 0;
+        while(y) {
+            res *= 10;
+            res += y % 10;
+            y /= 10;
+        }
 
-        string str1 = to_string(x), str2 = to_string(x);
-        reverse(str1.begin(), str1.end());
-        if(str1 == str2) return 1;
-        return 0;
+        if(res <= INT_MAX && res == x) return true;
+        return false;
     }
 };
