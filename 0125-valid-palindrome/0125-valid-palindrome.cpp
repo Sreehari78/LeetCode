51fb5a54d401a2e7c820ccfd775bdcc84d5aa430
Sreehari S <shraxx7@gmail.com>
2024-05-30 22:51:40 +0530

    Time: 7 ms (37.33%), Space: 9.8 MB (6.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+
         string s1 = "", s2 = "";
-        for(char it: s) if(isalpha(it) || isdigit(it)) s1 += tolower(it);
+
+        for (char it : s)
+            if (isalpha(it) || isdigit(it))
+                s1 += tolower(it);
+
         s2 = s1;
         reverse(s1.begin(), s1.end());
+        
         return s2 == s1;
     }
 };
