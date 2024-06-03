811249ce0ca0b58e995a02fe7dc29e0ac85646b0
Sreehari S <shraxx7@gmail.com>
2024-06-03 14:51:21 +0530

    Time: 7 ms (96.27%), Space: 19.2 MB (84.83%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
-        int ans = 0;
-        for(auto it: nums) ans ^= it;
-        return ans;
+        int x = nums[0];
+        for(int i = 1; i < nums.size(); i++) x ^= nums[i];
+        return x;
     }
 };
