2cc1e8cbce985e0c97261dd9fa28804f9e3be160
Sreehari S <shraxx7@gmail.com>
2024-06-09 13:10:05 +0530

    Time: 145 ms (49.71%), Space: 137 MB (32.09%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
+        vector<int> positive, negative;
+        int p = 0, n = 0;
+
+        for(int it: nums)  
+            if(it > 0) positive.push_back(it);
+            else negative.push_back(it);
         
-        // vector<int> positive, negative;
-        // int p = 0, n = 0;
-
-        // for(int it: nums) 
-        //     if(it > 0) positive.push_back(it); 
-        //     else negative.push_back(it);
-
-        // for(int i = 0; i < nums.size(); i++) 
-        //     if(i % 2 == 0) nums[i] = positive[p++]; 
-        //     else nums[i] = negative[n++];
-
-        // return nums;
-
-        int a = 0, b = 1, n = nums.size();
-        vector<int> ans(n);
-
-        for(int i = 0; i < n; i++) 
-            if(nums[i] > 0) {
-                ans[a] = nums[i];
-                a += 2;
-            }
-            else {
-                ans[b] = nums[i];
-                b += 2;
-            }
+        for(int i = 0; i < nums.size(); i++) 
+            if(i % 2 == 0) nums[i] = positive[p++];
+            else nums[i] = negative[n++];
         
-        return ans;
+        return nums;
     }
 };
