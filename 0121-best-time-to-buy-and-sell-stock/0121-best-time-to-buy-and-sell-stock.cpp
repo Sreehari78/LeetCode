3f60dee692df00dd79394b65a9f65f9256930e9f
Sreehari S <shraxx7@gmail.com>
2024-06-10 10:00:55 +0530

    Time: 55 ms (98.69%), Space: 96 MB (7.51%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,9 +4,9 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
-        
+
         int mini = prices[0], profit = 0;
-        for(int i = 0; i < prices.size(); i++) {
+        for (int i = 0; i < prices.size(); i++) {
             profit = max(profit, prices[i] - mini);
             mini = min(mini, prices[i]);
         }
