cf6200a2dc74ca9eab984fc0783095f4dfeb071c
Sreehari S <shraxx7@gmail.com>
2024-06-10 09:23:30 +0530

    Time: 43 ms (99.64%), Space: 95.9 MB (9.39%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        
         int mini = prices[0], profit = 0;
         for(int i = 0; i < prices.size(); i++) {
             profit = max(profit, prices[i] - mini);
