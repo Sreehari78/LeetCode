d724b1e906204563ece0c338030555c048eba5df
Sreehari S <shraxx7@gmail.com>
2024-06-13 09:13:55 +0530

    Time: 46 ms (98.05%), Space: 70 MB (92.63%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    int speedUp = [] {
+        std::ios::sync_with_stdio(0);
+        std::cin.tie(0);
+        return 0;
+    }();
+    
     int maxSubArray(vector<int>& nums) {
         int res = INT_MIN, sum = 0;
 
