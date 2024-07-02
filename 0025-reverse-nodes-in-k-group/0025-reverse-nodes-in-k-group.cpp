b12499a2070619e27e903846291f4477f01b5d5c
Sreehari S <shraxx7@gmail.com>
2024-07-03 00:49:25 +0530

    Time: 7 ms (86.35%), Space: 15.2 MB (10.85%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -11,6 +11,10 @@
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        
         ListNode* current = head;
         ListNode* left = nullptr;
         ListNode* prev = nullptr;
