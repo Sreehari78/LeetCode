7576e4db2e4d8a93b0c077f43955ea10d0fb8b68
Sreehari S <shraxx7@gmail.com>
2024-07-03 14:24:09 +0530

    Time: 5 ms (82.23%), Space: 9.6 MB (39.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    Solution() {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-    }
-    
     bool isAnagram(string s, string t) {
-        unordered_map <char, int> mp;
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        
+        unordered_map<char, int> mp;
 
-        for(auto it: s) mp[it]++;
-        for(auto it: t) mp[it]--;
-        for(auto it: mp) if(it.second != 0) return false;
+        for (char it : s)
+            mp[it]++;
+
+        for (char it : t)
+            if (!mp[it])
+                return false;
+            else
+                mp[it]--;
+
+        for(auto it: mp) if(it.second) return false;
         return true;
     }
 };
