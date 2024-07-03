764aa630131d1d11e2f8c7f470b3cef146f120c2
Sreehari S <shraxx7@gmail.com>
2024-07-03 14:03:29 +0530

    Time: 4 ms (89.97%), Space: 18.1 MB (7.05%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -4,6 +4,7 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
+        
         int left = 0, right = numbers.size() - 1;
 
         while (left < right) {
