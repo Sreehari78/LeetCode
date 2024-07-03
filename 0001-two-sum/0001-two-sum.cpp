bb7c033c81ee319199827bdccf5818e5f430c0fc
Sreehari S <shraxx7@gmail.com>
2024-07-03 15:00:59 +0530

    Time: 4 ms (94.08%), Space: 15.2 MB (9.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -6,7 +6,7 @@ public:
         for(int i = 0; i < nums.size(); i++) 
             if(mp[nums[i]]) return {i, mp[nums[i]] - 1};
             else mp[target - nums[i]] = i + 1;
-        
+
         return {};
     }
 };
