0331b2bc41c2a00d0f83a72b501b106bf84f0f37
Sreehari S <shraxx7@gmail.com>
2024-07-03 14:00:31 +0530

    Time: 6 ms (84.59%), Space: 18.1 MB (7.05%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
         int left = 0, right = numbers.size() - 1;
 
-        while(left < right) {
-            int sum = numbers[left] + numbers[right];
-
-            if(sum == target) return {++left, ++right};
-            else if(sum > target) right--;
-            else left++;
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target)
+                return {++left, ++right};
+            else if (numbers[left] + numbers[right] > target)
+                right--;
+            else
+                left++;
         }
 
         return {};
