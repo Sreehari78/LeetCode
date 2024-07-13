878c66c6ccdb00dfba754a90208a2640ef3fffa8
Sreehari S <shraxx7@gmail.com>
2024-07-13 16:22:41 +0530

    Time: 3 ms (81.56%), Space: 9.7 MB (56.46%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
+        stack<string> st;
+        string res = "", word = "";
 
-        stack<string> stk;
-        
-        s += ' ';
-        int n = s.length();
-        string str = "", result = "";
+        for (char it : s) 
 
-        for(int i = 0; i < n; i++) {
-            if(s[i] == ' ') {
-                if(str != "") stk.push(str);
-                str = ""; 
+            if (isalpha(it) || isdigit(it))
+                word += it;
+            else if (it == ' ' && word.length()) {
+                if (word != "")
+                    st.push(word);
+                word = "";
             }
-            else str += s[i];
+
+        if (word != "")
+            st.push(word);
+
+        while (st.size()) {
+            res += st.top();
+            st.pop();
+            if (st.size())
+                res += ' ';
         }
 
-        while(!stk.empty()) { 
-            result += stk.top();
-            stk.pop();
-            if(!stk.empty())
-                result += ' ';
-        }
-
-        return result;
+        return res;
     }
 };
