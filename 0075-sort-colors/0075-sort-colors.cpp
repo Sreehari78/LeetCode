efdf1d400d55ac792767a9efb5d42c1f8134b413
Sreehari S <shraxx7@gmail.com>
2024-07-15 10:27:59 +0530

    Time: 5 ms (15.89%), Space: 10.1 MB (8.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,9 +3,9 @@ public:
     void sortColors(vector<int>& nums) {
         int low = 0, high = nums.size() - 1;
 
-        for(int mid = low; mid <= high; mid++)  {
-            if(nums[mid] == 0) swap(nums[low++], nums[mid]);
-            else if(nums[mid] == 2) swap(nums[high--], nums[mid--]);
+        for(int mid = low; mid <= high; mid++) {
+            if(nums[mid] == 0) swap(nums[mid], nums[low++]);
+            else if(nums[mid] == 2) swap(nums[mid--], nums[high--]);
         }
     }
 };
