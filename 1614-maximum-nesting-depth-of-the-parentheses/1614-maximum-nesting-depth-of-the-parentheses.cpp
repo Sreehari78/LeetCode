c35c305899e7e93b74325a4f27999261726ec5c5
Sreehari S <shraxx7@gmail.com>
2024-07-15 15:54:58 +0530

    Time: 4 ms (8.93%), Space: 8 MB (8.27%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -4,11 +4,12 @@ public:
         stack<char> st;
         int res = 0;
 
-        for(char it: s) if(it == '(') {
-            st.push(it);
-            res < st.size() ? res = st.size() : res = res; 
-        }
-        else if(it == ')') st.pop();
+        for (char it : s)
+            if (it == '(') {
+                st.push(it);
+                res < st.size() ? res = st.size() : res = res;
+            } else if (it == ')')
+                st.pop();
 
         return res;
     }
