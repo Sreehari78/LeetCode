422d6c9379ff4178845747b8009bf40283f58939
Sreehari S <shraxx7@gmail.com>
2024-07-17 21:35:48 +0530

    Time: 0 ms (100%), Space: 8.3 MB (19.06%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -4,46 +4,66 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
-        
+
         vector<int> res;
         int i = 0, j = 0, count = 0;
 
-        while(count < matrix[0].size()) {
-            while(j < matrix[0].size()) {
-                if(matrix[i][j] < 101) res.push_back(matrix[i][j]);
-                else break;
+        while (count < matrix[0].size()) {
+            while (j < matrix[0].size()) {
+                if (matrix[i][j] < 101)
+                    res.push_back(matrix[i][j]);
+                else
+                    break;
+
                 matrix[i][j] = 101;
                 j++;
             }
+
             j--;
             i++;
-            while(i < matrix.size()) {
-                if(matrix[i][j] < 101) res.push_back(matrix[i][j]);
-                else break;
+
+            while (i < matrix.size()) {
+                if (matrix[i][j] < 101)
+                    res.push_back(matrix[i][j]);
+                else
+                    break;
+
                 matrix[i][j] = 101;
                 i++;
             }
+
             i--;
             j--;
-            while(j > -1) {
-                if(matrix[i][j] < 101) res.push_back(matrix[i][j]);
-                else break;
+
+            while (j > -1) {
+                if (matrix[i][j] < 101)
+                    res.push_back(matrix[i][j]);
+                else
+                    break;
+
                 matrix[i][j] = 101;
                 j--;
             }
+
             j++;
             i--;
-            while(i > -1) {
-                if(matrix[i][j] < 101) res.push_back(matrix[i][j]);
-                else break;
+
+            while (i > -1) {
+                if (matrix[i][j] < 101)
+                    res.push_back(matrix[i][j]);
+                else
+                    break;
+                    
                 matrix[i][j] = 101;
                 i--;
             }
+
             i++;
             j++;
+
             count++;
         }
-        return res;
 
+        return res;
     }
 };
