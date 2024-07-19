d9193a44e4b2dfefdd9ae5383eef3cb04eb66ab3
Sreehari S <shraxx7@gmail.com>
2024-07-19 08:51:11 +0530

    Time: 26 ms (98.8%), Space: 17.4 MB (22.25%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -6,23 +6,44 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
         
-        unordered_map<ListNode*, int> mp;
-        
-        while(headA) {
-            mp[headA] = 1;
-            headA = headA -> next;
+        ListNode* ptr1 = headA;
+        ListNode* ptr2 = headB;
+        int count1 = 0, count2 = 0;
+
+        while (ptr1->next) {
+            ptr1 = ptr1->next;
+            count1++;
         }
 
-        while(headB) {
-            if(mp[headB] == 1) return headB;
-            headB = headB -> next;
+        while (ptr2->next) {
+            ptr2 = ptr2->next;
+            count2++;
         }
 
+        while (count1 > count2) {
+            headA = headA->next;
+            count1--;
+        }
+
+        while (count2 > count1) {
+            headB = headB->next;
+            count2--;
+        }
+
+        while (headA) {
+            if (headA == headB)
+                return headA;
+
+            headA = headA->next;
+            headB = headB->next;
+        }
         return nullptr;
     }
 };
