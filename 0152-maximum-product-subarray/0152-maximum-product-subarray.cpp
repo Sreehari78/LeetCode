cfba5a6212c794a5065789219844a499ed0cd64a
Sreehari S <shraxx7@gmail.com>
2024-07-23 19:38:29 +0530

    Time: 3 ms (84.9%), Space: 16.5 MB (20.02%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
-        
-        int prefix=1, suffix=1, n = nums.size(), maxi=INT_MIN;
-        for(int i = 0; i < nums.size();i++)
-        {
-            if(prefix==0) prefix=1;
-            if(suffix==0) suffix=1;
+        if(nums.size() == 1) return nums[0];
+        double prefix = 1, suffix = 1, res = 0;
 
-            prefix*=nums[i];
-            suffix*=nums[n-i-1];
-            maxi=max(maxi , max(suffix,prefix));
+        for (int i = 0; i < nums.size(); i++) {
+            if (!prefix)
+                prefix = 1;
+
+            if(!suffix)
+                suffix = 1;
+
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[nums.size() - i - 1];
+            res = max(res, max(prefix, suffix));
         }
 
-        return maxi;
+        return res;
     }
 };
