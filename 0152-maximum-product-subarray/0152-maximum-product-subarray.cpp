3ac814c4c1c858025b4aea434795b58d89900f32
Sreehari S <shraxx7@gmail.com>
2024-07-23 19:38:56 +0530

    Time: 4 ms (61.09%), Space: 16.8 MB (5.01%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+
         if(nums.size() == 1) return nums[0];
         double prefix = 1, suffix = 1, res = 0;
 
