8d987ea7890cca7529a5a74430eed1c77ca0dee4
Sreehari S <shraxx7@gmail.com>
2024-07-27 19:38:56 +0530

    Time: 10 ms (86.84%), Space: 13.3 MB (60.96%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,48 +1,29 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
-        
-        if(s.length() <= 1) return s;
-        
-        int n = s.length(), st = 0, end = 0, max_length = 0;
+        string res = "";
+        for(int i = 0; i < s.length(); i++) {
+            int left = i, right = i;
 
-        for(int i = 0; i < n; i++)  {
-            int l = i, r = i;
-            while(l >= 0 && r < n)
-                if(s[l] == s[r]) {
-                    l--;
-                    r++;
-                }
-                else break;
-            
-            int length = r - l - 1;
-            if(length > max_length) {
-
-                max_length = length;
-                st = l + 1;
+            while(left > -1 && right < s.length() && s[left] == s[right]) {
+                right++;
+                left--; 
             }
+
+            if(res.length() < right - left - 1)
+                res = s.substr(left + 1, right - left - 1);
+
+            left = i, right = i + 1;
+
+            while(left > -1 && right < s.length() && s[left] == s[right]) {
+                right++;
+                left--; 
+            }
+
+            if(res.length() < right - left - 1)
+                res = s.substr(left + 1, right - left - 1);
         }
 
-        for(int i = 0; i < n; i++)  {
-            int l = i, r = i+1;
-            while(l >= 0 && r < n)
-                if(s[l] == s[r]) {
-                    l--;
-                    r++;
-                }
-                else break;
-            
-            int length = r - l - 1;
-            if(length > max_length) {
-
-                max_length = length;
-                st = l + 1;
-            }
-        }
-
-        return s.substr(st, max_length);
+        return res;
     }
 };
