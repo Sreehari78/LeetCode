b2967cb968cdbab12e4ec6aad66869512a7ab085
Sreehari S <shraxx7@gmail.com>
2024-07-31 11:27:50 +0530

    Time: 55 ms (99.06%), Space: 23.7 MB (73.71%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        
         int prefix = 0, suffix = 0, res = INT_MAX;
         if (s.length() == 1)
             return 0;
