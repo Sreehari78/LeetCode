4f07fc2e8efe52ef3968bc1fc690b52b2911e97d
Sreehari S <shraxx7@gmail.com>
2024-08-31 12:07:13 +0530

    Time: 8 ms (24.92%), Space: 13 MB (41.63%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,32 +1,33 @@
 class Solution {
-public:
-    void reccursion(int index, vector<int>& candidates, int remainingTarget, vector<int>& ans, vector<vector<int>>& result) {
+private:
+    vector<vector<int>> res;
+    vector<int> ans;
 
-        if(remainingTarget == 0) {
-            result.push_back(ans);
+    void helper(vector<int>& candidates, int target, int n) {
+        if(target < 0)
+            return;
+
+        if(target == 0) {
+            res.push_back(ans);
             return;
         }
 
-        for(int i = index; i < candidates.size(); i++) {
-            if(i > index && candidates[i] == candidates[i-1]) continue;
-            if(candidates[i] > remainingTarget) break;
+        for(int i = n; i < candidates.size(); i++) {
+            if(i > n && candidates[i] == candidates[i - 1])
+                continue;
+
+            if (candidates[i] > target)
+                break;
+
             ans.push_back(candidates[i]);
-            reccursion(i + 1, candidates, remainingTarget - candidates[i], ans, result);
+            helper(candidates, target - candidates[i], i + 1);
             ans.pop_back();
         }
     }
-
+public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
-
-        vector<vector<int>> result;
-        vector<int> ans;
-        
         sort(candidates.begin(), candidates.end());
-        reccursion(0, candidates, target, ans, result);
-        
-        return result;
+        helper(candidates, target, 0);
+        return res;
     }
 };
