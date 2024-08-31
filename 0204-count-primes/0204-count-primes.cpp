5b96d72deabc674d2c18b63a1901ed1713b550d0
Sreehari S <shraxx7@gmail.com>
2024-09-01 02:35:19 +0530

    Time: 416 ms (59.96%), Space: 11.9 MB (43.7%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -12,7 +12,7 @@ public:
             if(primes[i])
                 for(int j = i * i; j < n; j += i)
                     primes[j] = false;
-        
+
         for(int i = 2; i < n; i++)
             if(primes[i])
                 res++;
