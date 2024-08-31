56ffc6e46d586c00298f65f21b4411cf10aa2d7f
Sreehari S <shraxx7@gmail.com>
2024-09-01 02:31:57 +0530

    Time: 422 ms (56.92%), Space: 11.9 MB (35.41%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int countPrimes(int n) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+
         vector<bool> primes(n, true);
         int res = 0;
 
