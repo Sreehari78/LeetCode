2c32d32e321452ab0f2dd0ff9e748d7d803d0864
Sreehari S <shraxx7@gmail.com>
2024-09-02 12:48:17 +0530

    Time: 0 ms (100%), Space: 7.6 MB (89.26%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -4,10 +4,22 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
-        
-        for(int i = 0; i < haystack.size(); i++)
-            if(haystack.substr(i, needle.size()) ==  needle)
-                return i;
+
+        for(int i = 0; i < haystack.size(); i++) {
+            if(haystack[i] == needle[0]) {
+                int x = i, y = 0;
+
+                while(x < haystack.size() && y < needle.size()) {
+                    if(haystack[x] != needle[y])
+                        break;
+                    x++;
+                    y++;
+                }
+
+                if(y == needle.size())
+                    return i;
+            }
+        }
         
         return -1;
     }
