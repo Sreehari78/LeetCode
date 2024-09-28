14cfd0b89d0aee2ef1e5ce05115f2e431ff496ec
Sreehari S <shraxx7@gmail.com>
2024-09-28 10:53:43 +0530

    Time: 17 ms (57.77%), Space: 22 MB (88.08%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,35 +1,48 @@
 class Solution {
-public:      
+public:
+    bool isValidRow(vector<vector<char>>& board, int row, int col) {
+        char val = board[row][col];
+
+        for (int i = 0; i < 9; i++)
+            if (i != row && board[i][col] == val)
+                return false;
+
+        return true;
+    }
+
+    bool isValidCol(vector<vector<char>>& board, int row, int col) {
+        char val = board[row][col];
+
+        for (int i = 0; i < 9; i++)
+            if (i != col && board[row][i] == val)
+                return false;
+
+        return true;
+    }
+
+    bool isValidMat(vector<vector<char>>& board, int row, int col) {
+        char val = board[row][col];
+        int matRow = row / 3, matCol = col / 3;
+        int matRowStart = matRow * 3, matColStart = matCol * 3;
+
+        for (int i = matRowStart; i < matRowStart + 3; i++)
+            for (int j = matColStart; j < matColStart + 3; j++)
+                if ((i != row && j != col) && board[i][j] == val)
+                    return false;
+
+        return true;
+    }
+
     bool isValidSudoku(vector<vector<char>>& board) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
-        
-        vector<vector<char>> transpose(9, vector<char>(9));
-        vector<unordered_set<char>> rows(9), cols(9), boxes(9);
-
-        
-        for(int i = 0; i < 9; i++) for(int j = 0; j < 9; j++) transpose[i][j] = board[j][i]; 
-
-        //rows
-        for(vector<char> it: board) 
-            for(int j = 0; j < 9; j++) 
-                for(int k = j + 1; k < 9; k++) if(it[k] != '.' && it[j] == it[k]) return false;
-        
-        //columns
-        for(vector<char> it: transpose) 
-            for(int j = 0; j < 9; j++) 
-                for(int k = j + 1; k < 9; k++) if(it[k] != '.' && it[j] == it[k]) return false;
-        
-        //3x3 grids
-        for(int i = 0; i < 9; i++)
-            for(int j = 0; j < 9; j++) {
-                char cell = board[i][j];
-                if(cell == '.') continue;
-
-                int boxIndex = (i / 3) * 3 + j / 3;
-                if (boxes[boxIndex].count(cell)) return false;
-                boxes[boxIndex].insert(cell);
+        for (int row = 0; row < 9; row++)
+            for (int col = 0; col < 9; col++) {
+                bool res = true;
+                if (board[row][col] != '.')
+                    res = isValidRow(board, row, col) &&
+                          isValidCol(board, row, col) &&
+                          isValidMat(board, row, col);
+                if (!res)
+                    return false;
             }
 
         return true;
