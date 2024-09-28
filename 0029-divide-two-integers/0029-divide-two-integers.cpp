78a58cc3ababe6d0b9ecc37929fbbd882f620ecd
Sreehari S <shraxx7@gmail.com>
2024-09-28 11:07:37 +0530

    Time: 0 ms (100%), Space: 8.4 MB (35.11%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,27 +1,6 @@
 class Solution {
 public:
-    Solution() {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-    }
-
-    int divide(long dividend, long divisor) {
-        if(dividend == INT_MIN && divisor == -1) return INT_MAX;
-        if(dividend == INT_MIN && divisor == 1) return INT_MIN;
-        if(divisor == 1) return dividend;
-
-        long count = 0;
-        bool flag1 = false, flag2 = false;
-
-        if(dividend < 0) flag1 = true;
-        if(divisor < 0) flag2 = true;
-
-        dividend = abs(dividend);
-        divisor = abs(divisor);
-
-        for(; dividend >= divisor; dividend -= divisor) count++;
-        if(flag1 == flag2) return count;
-        else return -count;
+    int divide(int dividend, int divisor) {
+        return (long) dividend / divisor > INT_MAX ? INT_MAX : dividend / divisor;
     }
 };
