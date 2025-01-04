963f2474243ae8637b70dd3bfae74029e3b1c9ce
Sreehari S <shraxx7@gmail.com>
2025-01-04 09:58:25 +0530

    Time: 45 ms (79.07%), Space: 29.1 MB (58.59%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
-
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
-        
-        for(int left = 0; left < nums.size(); left++) {
-            if(left > 0 && nums[left] == nums[left - 1]) continue;
-            int mid = left + 1, right = nums.size() - 1;
 
-            while(mid < right) {
+        for(int left = 0; left < nums.size(); left++) {
+
+            if(left > 0 && nums[left] == nums[left - 1]) continue;
+            int mid = left + 1, right = nums.size() - 1;;
+
+            while(right > mid) {
                 int sum = nums[left] + nums[mid] + nums[right];
 
-                if(sum < 0) mid++;
-                else if(sum > 0) right--;
+                if (sum < 0) mid++;
+                else if (sum > 0) right--;
                 else {
                     res.push_back({nums[left], nums[mid++], nums[right--]});
-                    while(mid < right && nums[mid] == nums[mid - 1]) mid++;
-                    while(mid < right && nums[right] == nums[right + 1]) right--;
+                    while(right > mid && nums[mid] == nums[mid - 1]) mid++;
+                    while(right > mid && nums[right] == nums[right + 1]) right--;
                 }
-                
-            } 
+            }
         }
 
         return res;
