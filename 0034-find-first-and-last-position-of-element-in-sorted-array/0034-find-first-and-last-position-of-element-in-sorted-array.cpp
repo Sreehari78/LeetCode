113e17e729071253cc8eb8e5b3220afd8d3f761d
Sreehari S <shraxx7@gmail.com>
2025-05-20 22:49:19 +0530

    Time: 0 ms (100%), Space: 17.4 MB (85.95%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int start = findFirstOccurrence(nums, target);
-        int end = findLastOccurrence(nums, target);
+    int binarySearch(vector<int>& nums, int target, bool isFirst) {
+        int start = 0, end = nums.size() - 1, pos = -1;
 
-        return {start, end};
-    }
+        while(start <= end) {
+            int mid = (start + end) / 2;
 
-    int findFirstOccurrence(vector<int>& nums, int target) {
-        int start = 0, end = nums.size() - 1, result = -1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) {
-                result = mid;
-                end = mid - 1;
-            } 
-            else if (nums[mid] < target) start = mid + 1;
-            else end = mid - 1;
-        }
-
-        return result;
-    }
-
-    int findLastOccurrence(vector<int>& nums, int target) {
-        int start = 0, end = nums.size() - 1, result = -1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) {
-                result = mid;
-                start = mid + 1;
+            if(nums[mid] == target) {
+                pos = mid;
+                if(isFirst)
+                    end = mid - 1;
+                else 
+                    start = mid + 1;
             }
-            else if (nums[mid] < target) start = mid + 1;
-            else end = mid - 1;
+
+            else if(nums[mid] > target)
+                end = mid - 1;
+            else 
+                start = mid + 1;
         }
 
-        return result;
+        return pos;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {       
+        return {binarySearch(nums, target, true), binarySearch(nums, target, false)};
     }
 };
