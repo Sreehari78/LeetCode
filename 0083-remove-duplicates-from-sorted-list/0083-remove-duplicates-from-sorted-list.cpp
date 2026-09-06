de5f0d4ff84341bf8d4b1c9d831b618e66c28bc7
Sreehari S <shraxx7@gmail.com>
2026-09-06 22:21:59 +0530

    Time: 0 ms (100%), Space: 16.2 MB (68.83%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -11,18 +11,14 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        ListNode* ptr1;
-        ptr1 = head;
+        ListNode* ptr1 = head;
 
-        while(ptr1) {
-            cout<<ptr1->val << " ";
-
-            while(ptr1->next && ptr1->val == ptr1->next->val)
+        while (ptr1) {
+            while (ptr1->next && ptr1->val == ptr1->next->val)
                 ptr1->next = ptr1->next->next;
             ptr1 = ptr1->next;
         }
 
         return head;
-
     }
 };
