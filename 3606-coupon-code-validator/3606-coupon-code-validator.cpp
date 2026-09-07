72fa2ddc80053d17f0b874e0f6d0ee0be17ea28f
Sreehari S <shraxx7@gmail.com>
2026-09-08 00:23:57 +0530

    Time: 16 ms (6.73%), Space: 70 MB (34.53%) - LeetHub

---
+++ 3606-coupon-code-validator/3606-coupon-code-validator.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+        vector<pair<string, string>> ans;
+        vector<string> res;
+        unordered_set<string> businessLineCheck = {"electronics", "grocery", "pharmacy", "restaurant"};
+
+        for(int i = 0; i < isActive.size(); i++) {
+            bool isValid = true;
+
+            // code check
+            if(code[i] == "")
+                continue;
+            
+            for(char c : code[i])
+                if(!isalnum(c) && c != '_') {
+                    isValid = false;
+                    break;
+                }
+
+            // business line check
+            if(!businessLineCheck.count(businessLine[i]))
+                isValid = false;
+
+            // isActive line check
+            if(!isActive[i])
+                isValid = false;
+
+            if(isValid)
+                ans.push_back({code[i], businessLine[i]});
+        }  
+
+        sort(ans.begin(), ans.end(), [](const auto& a, const auto& b) { return a.second == b.second ? a.first < b.first : a.second < b.second; });
+
+        for(int i = 0; i < ans.size(); i++)
+            res.push_back(ans[i].first);
+
+        return res;
+    }
+};
