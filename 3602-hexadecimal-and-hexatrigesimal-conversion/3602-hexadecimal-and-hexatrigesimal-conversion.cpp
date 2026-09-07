4c858eecf0da9eae3a413a3652af1d5fde1fc855
Sreehari S <shraxx7@gmail.com>
2026-09-07 21:45:48 +0530

    Time: 3 ms (19.23%), Space: 8.2 MB (77.88%) - LeetHub

--- 3602-hexadecimal-and-hexatrigesimal-conversion/3602-hexadecimal-and-hexatrigesimal-conversion.cpp
+++ 3602-hexadecimal-and-hexatrigesimal-conversion/3602-hexadecimal-and-hexatrigesimal-conversion.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     string concatHex36(int n) {
-        string hexd = "", hext = "", dataset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";\
-        int num1 = n*n, num2 = n*n*n;
+        string hexd = "", hext = "", 
+        dataset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int num1 = n * n, num2 = n * n * n;
 
-        while(num1) {
-            hexd += dataset[num1%16];
+        while (num1) {
+            hexd += dataset[num1 % 16];
             num1 /= 16;
         }
         reverse(hexd.begin(), hexd.end());
-        
-        while(num2) {
-            hext += dataset[num2%36];
+
+        while (num2) {
+            hext += dataset[num2 % 36];
             num2 /= 36;
         }
         reverse(hext.begin(), hext.end());
 
-        return hexd+hext;
+        return hexd + hext;
     }
 };
