536d649fa7f4b98812fd747bf8f02e62baf6d9f7
Sreehari S <shraxx7@gmail.com>
2026-09-08 19:30:56 +0530

    Time: 2 ms (13.69%), Space: 55.5 MB (12.11%) - LeetHub

--- 4024-nearest-available-drone/4024-nearest-available-drone.cpp
+++ 4024-nearest-available-drone/4024-nearest-available-drone.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int nearestDrone(vector<vector<int>>& drones, vector<int>& target) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+
         int res = -1, mini = INT_MAX;
         
         for(int i = 0; i < drones.size(); i++) {
             int distance = abs(drones[i][0] - target[0]) + abs(drones[i][1] - target[1]);
-            cout<<distance<<" "<<drones[i][2]<<endl;
 
             if(drones[i][2] >= distance)
                 if(mini > distance) {
                     mini = distance;
                     res = i;
                 }
-
         }
 
         return (res == INT_MAX)? -1: res;
