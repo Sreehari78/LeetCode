0affbd59135b3653db655c8aa94947b843aa4574
Sreehari S <shraxx7@gmail.com>
2026-09-12 14:02:35 +0530

    Time: 3 ms (21.4%), Space: 9.5 MB (22.35%) - LeetHub

---
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int zeros = 0, ones = 0, res = 0;
+        for(int i = 0; i < s.length(); i++) {
+            (s[i] == '0')? zeros++: ones++;
+
+            if((zeros - ones) >= -1 && (zeros - ones <= 1)) res++;
+        }
+
+        return res;
+    }
+};
