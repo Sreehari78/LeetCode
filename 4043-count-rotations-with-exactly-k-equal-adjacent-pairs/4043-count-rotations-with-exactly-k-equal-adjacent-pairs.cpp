960748793be9204347bb28c62ac03680814c21a8
Sreehari S <shraxx7@gmail.com>
2026-09-12 13:27:16 +0530

    Time: 7 ms (54.2%), Space: 9.5 MB (59.2%) - LeetHub

--- 4043-count-rotations-with-exactly-k-equal-adjacent-pairs/4043-count-rotations-with-exactly-k-equal-adjacent-pairs.cpp
+++ 4043-count-rotations-with-exactly-k-equal-adjacent-pairs/4043-count-rotations-with-exactly-k-equal-adjacent-pairs.cpp
@@ -17,9 +17,9 @@ public:
             if (score == k)
                 res++;
             
-            char prev = s[0];
+            char rotate = s[0];
             s.erase(0,1);
-            s.push_back(prev);
+            s.push_back(rotate);
         }
 
         return res;
