47bcead00924bd36515debdc93d490d9db38d42b
Sreehari S <shraxx7@gmail.com>
2026-09-12 14:02:45 +0530

    Time: 0 ms (100%), Space: 9.6 MB (7.86%) - LeetHub

--- 4006-count-valid-prefixes/4006-count-valid-prefixes.cpp
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    
     int countValidPrefixes(string s) {
         int zeros = 0, ones = 0, res = 0;
         for(int i = 0; i < s.length(); i++) {
