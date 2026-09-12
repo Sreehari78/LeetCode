b8cfdd34025f323a91ae470cd5a3727295f6b385
Sreehari S <shraxx7@gmail.com>
2026-09-12 13:28:29 +0530

    Time: 5 ms (56.55%), Space: 9.5 MB (77.37%) - LeetHub

--- 4043-count-rotations-with-exactly-k-equal-adjacent-pairs/4043-count-rotations-with-exactly-k-equal-adjacent-pairs.cpp
+++ 4043-count-rotations-with-exactly-k-equal-adjacent-pairs/4043-count-rotations-with-exactly-k-equal-adjacent-pairs.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
-    int countRotations(string s, int k) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
 
+    int countRotations(string s, int k) {
         int res = 0, n = s.length();
 
         for (int i = 0; i < n; i++) {
