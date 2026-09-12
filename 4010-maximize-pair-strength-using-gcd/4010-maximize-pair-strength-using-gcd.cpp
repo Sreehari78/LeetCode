0fceb0f96eedd4feb188dc38ca585924e13c200c
Sreehari S <shraxx7@gmail.com>
2026-09-12 13:44:46 +0530

    Time: 819 ms (46.8%), Space: 37.4 MB (7.89%) - LeetHub

--- 4010-maximize-pair-strength-using-gcd/4010-maximize-pair-strength-using-gcd.cpp
+++ 4010-maximize-pair-strength-using-gcd/4010-maximize-pair-strength-using-gcd.cpp
@@ -4,7 +4,7 @@ public:
         long long res = 0;
 
         for(int i = 0; i < nums.size(); i++)
-            for(int j = 0; j < nums.size(); j++) {
+            for(int j = i + 1; j < nums.size(); j++) {
                 if(i == j) continue;
 
                 long long g = gcd(nums[i], nums[j]);
