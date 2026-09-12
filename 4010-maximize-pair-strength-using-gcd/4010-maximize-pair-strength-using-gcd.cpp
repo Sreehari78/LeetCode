5e65196d59349010d97dac48e5ce449e4ff0629b
Sreehari S <shraxx7@gmail.com>
2026-09-12 13:41:15 +0530

    Time: 1807 ms (5%), Space: 37.5 MB (7.89%) - LeetHub

---
+++ 4010-maximize-pair-strength-using-gcd/4010-maximize-pair-strength-using-gcd.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    
+    long long maxPairStrength(vector<int>& nums) {
+        long long res = 0;
+
+        for(int i = 0; i < nums.size(); i++)
+            for(int j = 0; j < nums.size(); j++) {
+                if(i == j) continue;
+
+                long long ans = (1LL * nums[i] * nums[j]) / pow(__gcd(nums[i], nums[j]), 2);
+                res = max(res, ans);
+            }
+
+        return res;
+    }
+};
